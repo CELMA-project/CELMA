54b7240a7854c719a6dc0e74940e42618639cfed
Michael Løiten <michael.l.magnussen@gmail.com>
2016-05-06 15:44:25 +0200

    Modifications to TopDown folder
    Former-commit-id: 55fafd79be613b8721a614adeed781be8613bac6

--- celma/1.0-CELMATopDown/PBSDriver-0-d-0-WiderS.py
+++ celma/1.0-CELMATopDown/PBSDriver-0-d-0-WiderS.py
@@ -16,14 +16,14 @@ from postProcessing.plotting import combined1D2D
 # The options for the run
 # =============================================================================
 # *****************************************************************************
-eiCollisions = [600, 300, 100, 50, 1]
+eiCollisions = [100, 50, 10, 5, 1, 0.1]
 # *****************************************************************************
 # Set the temporal domain
 restart    = None
 remove_old = False
 nout       = [20]
 timestep   = [5e0]
-directory  = "b-profileLnN"
+directory  = "d-widerS"
 # Shall we make?
 make       = False
 # =============================================================================
@@ -38,7 +38,7 @@ ySlice     = 8
 zSlice     = 0
 showPlot   = False
 savePlot   = True
-theRunName = "0-b-0-profileLnNCollScan"
+theRunName = "0-d-0-widerS"
 # =============================================================================
 
 
