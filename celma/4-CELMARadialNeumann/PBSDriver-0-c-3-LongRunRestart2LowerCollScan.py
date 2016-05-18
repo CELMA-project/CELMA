7dc1e0bbde770b8864f2bb952194ff00b1328af4
Michael Løiten <michael.l.magnussen@gmail.com>
2016-05-18 19:10:19 +0200

    Added new drivers
    Former-commit-id: ebed72226bd2e0e7d8a85ae5486ea0eb0ffc8b2f

--- celma/4-CELMARadialNeumann/PBSDriver-0-c-3-LongRunRestart2LowerCollScan.py
+++ celma/4-CELMARadialNeumann/PBSDriver-0-c-3-LongRunRestart2LowerCollScan.py
@@ -16,13 +16,13 @@ from postProcessing.plotting import combined1D2D
 # The options for the run
 # =============================================================================
 # *****************************************************************************
-eiCollisions = [200, 100, 50, 10]
+eiCollisions = [90, 80, 70, 60, 50, 10, 1, 0.1]
 # *****************************************************************************
 # Set the temporal domain
 restart    = "overwrite"
 # Uncomment this if you just want to plot
 # restart      = None;
-restart_from = "c-smallerCylinderNoDamp/nout_20_timestep_50.0/cst_nuEI_300_tag_0-c-1-CollisionScan_0/"
+restart_from = "c-smallerCylinderNoDamp/nout_20_timestep_5.0/cst_nuEI_100_tag_0-c-2-LongRunRestartLowerCollScan_0/"
 remove_old = False
 nout       = [20]
 timestep   = [5e0]
@@ -41,7 +41,7 @@ ySlice     = 8
 zSlice     = 0
 showPlot   = False
 savePlot   = True
-theRunName = "0-c-2-LongRunRestartLowerCollScan"
+theRunName = "0-c-3-LongRunRestart2LowerCollScan"
 # =============================================================================
 
 
