4bc1d07cc5c03e6d0ddd9b17461ff2418c2ba323
loeiten <michael.l.magnussen@gmail.com>
2016-05-19 19:47:07 +0200

    New folder - Add noise
    Former-commit-id: fe5929ff8221cb65eb8df23e31cf772675626ac0

---
+++ celma/5-CELMAWNoise/PBSDriver-0-a-0-CollScan.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""Driver which runs using PBS."""
+
+from bout_runners import PBS_runner
+import numpy as np
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath('./../common/python')
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from postProcessing.plotting import combined1D2D
+
+# The options for the run
+# =============================================================================
+# *****************************************************************************
+eiCollisions = [600, 300, 100]
+# *****************************************************************************
+# Set the temporal domain
+restart    = None
+remove_old = False
+nout       = [20, 20]
+timestep   = [5e-1, 5e0]
+directory  = "a-data"
+# Shall we make?
+make       = False
+# =============================================================================
+
+
+# The options for the post processing function
+# =============================================================================
+xguards    = False
+yguards    = False
+xSlice     = 0
+ySlice     = 8
+zSlice     = 0
+showPlot   = False
+savePlot   = True
+theRunName = "0-a-0-CollScan"
+# =============================================================================
+
+
+# The PBS options
+# =============================================================================
+# Specify the numbers used for the BOUT runs
+nproc                 = 96
+BOUT_nodes            = 5
+BOUT_ppn              = 20
+BOUT_walltime         = '06:00:00'
+BOUT_run_name         = theRunName
+post_process_nproc    = 1
+post_process_nodes    = 1
+post_process_ppn      = 20
+post_process_walltime = '0:29:00'
+post_process_queue    = 'xpresq'
+post_process_run_name = 'post' + theRunName.capitalize()
+# =============================================================================
+
+
+# Create the runner
+# =============================================================================
+myRuns = PBS_runner(\
+            directory  = directory ,\
+            nproc      = nproc ,\
+            # Set temporal domain
+            nout       = nout  ,\
+            timestep   = timestep,\
+            # Copy the source file
+            cpy_source = True  ,\
+            make       = make  ,\
+            restart    = restart,\
+            additional = [
+                          ('tag',theRunName,0),\
+                          ('cst','nuEI',eiCollisions),\
+                         ],\
+            # PBS options
+            BOUT_nodes            = BOUT_nodes           ,\
+            BOUT_ppn              = BOUT_ppn             ,\
+            BOUT_walltime         = BOUT_walltime        ,\
+            BOUT_run_name         = BOUT_run_name        ,\
+            post_process_nproc    = post_process_nproc   ,\
+            post_process_nodes    = post_process_nodes   ,\
+            post_process_ppn      = post_process_ppn     ,\
+            post_process_walltime = post_process_walltime,\
+            post_process_queue    = post_process_queue   ,\
+            post_process_run_name = post_process_run_name,\
+            )
+# =============================================================================
+
+
+# Perform the run
+# =============================================================================
+myRuns.execute_runs(\
+                     remove_old               = remove_old,\
+                     post_processing_function = combined1D2D,\
+                     # This function will be called every time after
+                     # performing a run
+                     post_process_after_every_run = True,\
+                     # Below are the kwargs arguments being passed to
+                     # the post processing function
+                     # Switches
+                     xguards        = xguards           ,\
+                     yguards        = yguards           ,\
+                     xSlice         = xSlice            ,\
+                     ySlice         = ySlice            ,\
+                     zSlice         = zSlice            ,\
+                     savePlot       = savePlot          ,\
+                     saveFolderFunc = "scanWTagSaveFunc",\
+                     theRunName     = theRunName        ,\
+                    )
+# =============================================================================
