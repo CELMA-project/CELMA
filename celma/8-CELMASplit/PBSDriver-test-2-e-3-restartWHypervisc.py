8664519d9941b297b6c6da5d822d3ac2002fe1f6
Michael Løiten <michael.l.magnussen@gmail.com>
2016-06-29 17:52:42 +0200

    New runners
    Former-commit-id: 7943f52ebe5c0f168f009ea3fea339db3a8488c2

--- celma/8-CELMASplit/PBSDriver-test-2-e-3-restartWHypervisc.py
+++ celma/8-CELMASplit/PBSDriver-test-2-e-3-restartWHypervisc.py
@@ -16,19 +16,20 @@ from postProcessing.plotting import combinedDriver
 # The options for the run
 # =============================================================================
 # *****************************************************************************
-ownOpType           = "3Brackets"
+ownOpType           = "3BasicBrackets"
 ownFilterType       = "radialLowPass"
 sAmp                = [0.020]
 saveDdt             = True
 includeNoise        = False
 forceAddNoise       = False
-useHyperViscAzVortD = False
+useHyperViscAzVortD = True
+artHyperAzVortD     = [1e-1, 1e0]
 # *****************************************************************************
 remove_old = False
 restart    = "overwrite"
 # Uncomment this if you just want to plot
 # restart      = None;
-restart_from = "f-moreSource3Brackets/nout_300_timestep_10/nz_128/ownFilters_type_radialLowPass_ownOperators_type_3Brackets_switch_forceAddNoise_True_switch_includeNoise_True_switch_saveDdt_True_switch_useHyperViscAzVortD_False_tag_2-e-2-moreSourceAddNoiseRadialLowPass3Brackets_0_theSource_a_0.02/"
+restart_from = "f-moreSource3Brackets/nout_300_timestep_10/nz_128/ownFilters_type_radialLowPass_ownOperators_type_3BasicBrackets_switch_forceAddNoise_True_switch_includeNoise_True_switch_saveDdt_True_switch_useHyperViscAzVortD_False_tag_2-e-3-moreSourceAddNoiseRadialLowPass3BasicBrackets_0_theSource_a_0.02/"
 # Set the spatial domain
 nz = 128
 # Set the temporal domain
@@ -49,7 +50,7 @@ ySlice     = 8
 zSlice     = 0
 showPlot   = False
 savePlot   = True
-theRunName = "I2-e-2-moreSourceRadialLowPass3Brackets"
+theRunName = "test-2-e-3-restartWHypervisc"
 # =============================================================================
 
 
@@ -59,7 +60,7 @@ theRunName = "I2-e-2-moreSourceRadialLowPass3Brackets"
 nproc                 = 24
 BOUT_nodes            = 2
 BOUT_ppn              = 12
-BOUT_walltime         = '03:00:00'
+BOUT_walltime         = '06:00:00'
 BOUT_run_name         = theRunName
 post_process_nproc    = 1
 post_process_nodes    = 1
@@ -93,6 +94,7 @@ myRuns = PBS_runner(\
                           ('switch'      , 'includeNoise'       , includeNoise ),\
                           ('switch'      , 'forceAddNoise'      ,forceAddNoise),\
                           ('switch'      , 'useHyperViscAzVortD',useHyperViscAzVortD),\
+                          ('cst'         , 'artHyperAzVortD'    ,artHyperAzVortD),\
                          ],\
             # PBS options
             BOUT_nodes            = BOUT_nodes           ,\
