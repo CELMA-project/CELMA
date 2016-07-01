db8fa987dd70d907ca115fd8b1bc44486581612e
Michael Løiten <michael.l.magnussen@gmail.com>
2016-07-01 14:34:23 +0200

    8.3 and 9.1 added
    Former-commit-id: 881140086f57cb923eacec37397b046b884c5132

---
+++ celma/9.1-CELMAUpdateKeepDivN/PBSDriver-2-a-0-addNoise.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+"""Driver which runs using PBS."""
+
+from bout_runners import PBS_runner
+import numpy as np
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath('./../common/python')
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from postProcessing.plotting import combinedDriver
+
+# The options for the run
+# =============================================================================
+# *****************************************************************************
+includeNoise        = True
+forceAddNoise       = True
+useHyperViscAzVortD = [False, True]
+# *****************************************************************************
+remove_old = False
+restart    = "overwrite"
+# Uncomment this if you just want to plot
+# restart      = None;
+restart_from = "../9-CELMAUpdateArtViscCleanUp/a-data/nout_20_timestep_5.0/nz_128/ownFilters_type_none_tag_1-a-0-expand_0/"
+# Set the spatial domain
+nz = 128
+# Set the temporal domain
+nout       = [300]
+timestep   = [10]
+directory  = "a-data"
+# Shall we make?
+make       = False
+# =============================================================================
+
+
+# The options for the post processing function
+# =============================================================================
+xguards    = False
+yguards    = False
+xSlice     = 0
+ySlice     = 8
+zSlice     = 0
+tSlice     = slice(-20, None)
+showPlot   = False
+savePlot   = True
+theRunName = "2-a-0-addNoise"
+# =============================================================================
+
+
+# The PBS options
+# =============================================================================
+# Specify the numbers used for the BOUT runs
+nproc                 = 24
+BOUT_nodes            = 2
+BOUT_ppn              = 12
+BOUT_walltime         = '48:00:00'
+BOUT_run_name         = theRunName
+post_process_nproc    = 1
+post_process_nodes    = 1
+post_process_ppn      = 20
+post_process_walltime = '0:29:00'
+post_process_queue    = 'xpresq'
+post_process_run_name = 'post' + theRunName.capitalize()
+# =============================================================================
+
+
+# Create the runner
+# =============================================================================
+myRuns = PBS_runner(\
+            directory  = directory ,\
+            nproc      = nproc ,\
+            # Set spatial domain
+            nz         = nz,\
+            # Set temporal domain
+            nout       = nout  ,\
+            timestep   = timestep,\
+            # Copy the source file
+            make       = make  ,\
+            restart    = restart,\
+            restart_from = restart_from,\
+            additional = [
+                          ('tag',theRunName,0),\
+                          ('switch'      , 'includeNoise'       , includeNoise ),\
+                          ('switch'      , 'forceAddNoise'      ,forceAddNoise),\
+                          ('switch'      , 'useHyperViscAzVortD',useHyperViscAzVortD),\
+                         ],\
+            # PBS options
+            BOUT_nodes            = BOUT_nodes           ,\
+            BOUT_ppn              = BOUT_ppn             ,\
+            BOUT_walltime         = BOUT_walltime        ,\
+            BOUT_run_name         = BOUT_run_name        ,\
+            post_process_nproc    = post_process_nproc   ,\
+            post_process_nodes    = post_process_nodes   ,\
+            post_process_ppn      = post_process_ppn     ,\
+            post_process_walltime = post_process_walltime,\
+            post_process_queue    = post_process_queue   ,\
+            post_process_run_name = post_process_run_name,\
+            )
+# =============================================================================
+
+
+# Perform the run
+# =============================================================================
+myRuns.execute_runs(\
+                     remove_old               = remove_old,\
+                     post_processing_function = combinedDriver,\
+                     # This function will be called every time after
+                     # performing a run
+                     post_process_after_every_run = True,\
+                     # Below are the kwargs arguments being passed to
+                     # the post processing function
+                     # Switches
+                     xguards        = xguards           ,\
+                     yguards        = yguards           ,\
+                     xSlice         = xSlice            ,\
+                     ySlice         = ySlice            ,\
+                     zSlice         = zSlice            ,\
+                     tSlice         = tSlice            ,\
+                     savePlot       = savePlot          ,\
+                     saveFolderFunc = "scanWTagSaveFunc",\
+                     theRunName     = theRunName        ,\
+                    )
+# =============================================================================
---
+++ celma/9.1-CELMAUpdateKeepDivN/celma.hxx
@@ -0,0 +1,128 @@
+// *************** Simulation of Celma *********************
+/* Geometry
+ *  x - The radial coordinate (rho)      [nx and dx set from the grid file]
+ *  y - The height of the cylinder (z)   [ny and dy set from the grid file]
+ *  z - The azimuthal coordinate (theta) [nz and dz set from the grid file and
+ *                                        internally in the BOUT++ framework]
+ */
+
+#ifndef __CELMA_H__
+#define __CELMA_H__
+
+#include <bout/physicsmodel.hxx>
+#include <invert_laplace.hxx>                   // Gives invert laplace option
+#include <field_factory.hxx>                    // Gives field factory
+#include <derivs.hxx>                           // Gives the bracket method
+#include <difops.hxx>                           // Gives the diff options
+#include <vecops.hxx>                           // Gives the vec diff options
+// Gives own boundaries (doing so by setting ghost points)
+#include "../common/c/include/ownBCs.hxx"
+// Gives own operators
+#include "../common/c/include/ownOperators.hxx"
+// Gives own laplacian inversions
+#include "../common/c/include/ownLaplacianInversions.hxx"
+// Gives the noise generator
+#include "../common/c/include/noiseGenerator.hxx"
+// Gives own lowPass filter
+#include "../common/c/include/ownFilters.hxx"
+
+class Celma : public PhysicsModel
+{
+protected:
+    int init(bool restarting);
+    int convective(BoutReal t);
+    int diffusive(BoutReal t, bool linear);
+private:
+    // Global variable initialization
+    // ############################################################################
+    // Evolved variables
+    // *****************************************************************************
+    Field3D uEPar, uIPar;     // Parallel velocities
+    Field3D lnN;               // Logarithm of density
+    Field3D vortD;              // Vorticity like quantity
+    // *****************************************************************************
+
+    // Non-evolved stored fields
+    // *****************************************************************************
+    Field3D phi, vort;      // Potential and vorticity
+    // *****************************************************************************
+
+    // Fields making up the evolved variables which are going to be stored
+    // *****************************************************************************
+    // lnN fields
+    Field3D lnNAdv, lnNRes, gradUEPar;
+    Field3D lnNUeAdv, srcN, lnNParArtVisc;
+    // uEPar fields
+    Field3D uEParAdv, uEParParAdv, gradPhiLnN;
+    Field3D uEParRes, ueNeutral, ueSrc;
+    Field3D uEParParArtVisc, uEParPerpArtVisc;
+    // uIPar fields
+    Field3D uIParAdv, uIParParAdv, gradPhi;
+    Field3D uIParRes, uiNeutral, uiSrc;
+    Field3D uIParParArtVisc, uIParPerpArtVisc;
+    // Vorticity fields
+    Field3D vortNeutral, potNeutral;
+    Field3D parDerDivUIParNGradPerpPhi;
+    Field3D vortDAdv, kinEnAdvN;
+    Field3D divParCur, vortDParArtVisc, vortDPerpArtVisc;
+    Field3D vortDhyperVisc;
+    // *****************************************************************************
+
+    // Temporary fields
+    // *****************************************************************************
+    Field3D DivUIParNGradPerpPhi;
+    // *****************************************************************************
+
+    // Auxiliary fields
+    // *****************************************************************************
+    Field3D n;               // Density
+    Field3D dampingProfile;  // Radial profile
+    Field3D S;               // Particle source
+    Field3D invJ;            // 1/J (used in front of the bracket operator)
+    Vector3D gradPerpLnN;    // gradPerpLnN
+    // *****************************************************************************
+
+    // Constants
+    // *****************************************************************************
+    BoutReal mu, Lambda;            // i-e mass ratio and ln(sqrt(mu/(2*PI)))
+    BoutReal nuEI, nuEN, nuIN;      // electron, ion, and neutral collision freqs
+    BoutReal a, bRho, bZ, cRho, cZ; // Variables used in the source term
+    BoutReal Lx, Ly;                // The box dimensions
+    // Parallel aritifical dissipation
+    BoutReal artViscParLnN, artViscParUEPar, artViscParUIPar;
+    BoutReal artViscParVortD;
+    // Perpendicular dissipation
+    BoutReal artViscPerpUEPar, artViscPerpUIPar;
+    BoutReal artViscPerpVortD;
+    // Azimuthal hyperviscosities
+    BoutReal artHyperAzVortD;
+    // *****************************************************************************
+
+    // Additional methods and solvers
+    // *****************************************************************************
+    BRACKET_METHOD bm;              // The bracket method
+    OwnBCs ownBC;                   // Class containing methods which sets the ghost points
+    OwnOperators *ownOp;            // Pointer to the chosen operators class
+    OwnLaplacianInversions ownLapl; // Class containing own laplacian
+    OwnFilters *ownFilter;          // Pointer to the chosen filter class
+    // *****************************************************************************
+
+    // Switches
+    // *****************************************************************************
+    string  ownOpType;           // Type of own operators
+    bool saveDdt;             // If ddt's should be saved
+    bool saveTerms;           // If terms should be saved
+    bool includeNoise;        // Include noise
+    bool forceAddNoise;       // Add noise on restart as well
+    bool noiseAdded;          // A check whether the noise is added or not
+    bool useHyperViscAzVortD; // If hyperviscosity should be used in the vorticity
+    // *****************************************************************************
+
+    // Make a field group to communicate
+    // *****************************************************************************
+    FieldGroup comGroup;
+    // *****************************************************************************
+    // ############################################################################
+};
+
+#endif
