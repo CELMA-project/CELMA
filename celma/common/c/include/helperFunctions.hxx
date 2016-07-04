6eb3c765923a9abf04af02ad6b60c306e1b3d837
loeiten <michael.l.magnussen@gmail.com>
2016-07-04 18:07:10 +0200

    Started implementation of monitors
    Former-commit-id: 1b1b4853f3624a8cb88fe59389b8268b7903ca65

---
+++ celma/common/c/include/helperFunctions.hxx
@@ -0,0 +1,16 @@
+#ifndef __HELPERFUNCTIONS_H__
+#define __HELPERFUNCTIONS_H__
+
+#include <bout.hxx>
+#include <fft.hxx>
+
+//! Function which returns the poloidal average of a field
+Field3D const polAvg(Field3D const &f, int const &xInd, int const &yInd);
+
+// Monitors
+//! Energy monitor
+int energyIntMon(Solver *solver, BoutReal simtime, int iter, int NOUT)
+
+#include "../src/helperFunctions.cxx"
+
+#endif
