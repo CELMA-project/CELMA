27d102bd701b0284fb2c8bda075a6a6ff1df6078
Michael Løiten <michael.l.magnussen@gmail.com>
2016-07-06 09:14:40 +0200

    New and updated drivers

--- celma/8.3-CELMASplitCleanUp/PBSDriver-b.1-0-R8-2e31-testAsOriginalNeumannO4.py
+++ celma/8.3-CELMASplitCleanUp/PBSDriver-b.1-0-R8-2e31-testAsOriginalNeumannO4.py
@@ -17,20 +17,20 @@ from postProcessing.plotting import combinedDriver
 # =============================================================================
 # *****************************************************************************
 useHyperViscAzVortD = True
-artViscParVortD     = 0
-artViscPerpVortD    = [5e-3, 5e-4, 5e-5]
+artViscParVortD  = 0.0
+artViscPerpVortD = 0.0
 # *****************************************************************************
 remove_old = False
 restart    = "overwrite"
 # Uncomment this if you just want to plot
 # restart      = None;
-restart_from = "../8-CELMASplit/f-moreSource3Brackets/nout_300_timestep_10/nz_128/cst_artHyperAzVortD_1.0_ownFilters_type_radialLowPass_ownOperators_type_BasicBrackets_switch_forceAddNoise_True_switch_includeNoise_True_switch_saveDdt_True_switch_useHyperViscAzVortD_True_tag_2-e-3.1-moreSAddNRadialLPHyperVBBrackets_0_theSource_a_0.02/"
+restart_from = "/mnt/mimer/mmag/CELMA-dev/celma/8-CELMASplit/f-moreSource3Brackets/nout_300_timestep_10/nz_128/cst_artHyperAzVortD_1.0_ownFilters_type_radialLowPass_ownOperators_type_BasicBrackets_switch_forceAddNoise_True_switch_includeNoise_True_switch_saveDdt_True_switch_useHyperViscAzVortD_True_tag_2-e-3.1-moreSAddNRadialLPHyperVBBrackets_0_theSource_a_0.02/"
 # Set the spatial domain
 nz = 128
 # Set the temporal domain
 nout       = [20, 300]
 timestep   = [10, 10]
-directory  = "a-data"
+directory  = "b.1-longerNeumannO4"
 # Shall we make?
 make       = False
 # =============================================================================
@@ -46,7 +46,7 @@ zSlice     = 0
 tSlice     = slice(-20, None)
 showPlot   = False
 savePlot   = True
-theRunName = "a-4-R8-2e31-addViscOnlyPerpScan"
+theRunName = "b.1-0-R8-2e31-testAsOriginalNeumannO4"
 # =============================================================================
 
 
@@ -56,7 +56,7 @@ theRunName = "a-4-R8-2e31-addViscOnlyPerpScan"
 nproc                 = 24
 BOUT_nodes            = 2
 BOUT_ppn              = 12
-BOUT_walltime         = '48:00:00'
+BOUT_walltime         = '24:00:00'
 BOUT_run_name         = theRunName
 post_process_nproc    = 1
 post_process_nodes    = 1
@@ -83,9 +83,9 @@ myRuns = PBS_runner(\
             restart_from = restart_from,\
             additional = [
                           ('tag',theRunName,0),\
-                          ('switch'      , 'useHyperViscAzVortD',useHyperViscAzVortD),\
-                          ('cst'         , 'artViscParVortD'     ,artViscParVortD),\
-                          ('cst'         , 'artViscPerpVortD'    ,artViscPerpVortD),\
+                          ('switch', 'useHyperViscAzVortD', useHyperViscAzVortD),\
+                          ('cst'   , 'artViscParVortD'    , artViscParVortD),\
+                          ('cst'   , 'artViscPerpVortD'   , artViscPerpVortD),\
                          ],\
             # PBS options
             BOUT_nodes            = BOUT_nodes           ,\
