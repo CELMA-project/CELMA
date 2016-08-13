c2f5ddbee757b8916f0f76b8e0e1090a6927e5fd
loeiten <michael.l.magnussen@gmail.com>
2016-08-13 19:52:43 +0200

    Uploaded unfinished surface integral

---
+++ MES/integrals/surfaceEdgeIntegral/SurfEdgeIntCyl.hxx
@@ -0,0 +1,54 @@
+// *************** Simulation of SurfEdgeIntCyl *********************
+/* Geometry
+ *  x - The radial coordinate (rho)      [nx and dx set from the grid file]
+ *  y - The height of the cylinder (z)   [ny and dy set from the grid file]
+ *  z - The azimuthal coordinate (theta) [nz and dz set from the grid file and
+ *                                        internally in the BOUT++ framework]
+ */
+
+#ifndef __SurfEdgeIntCyl_H__
+#define __SurfEdgeIntCyl_H__
+
+#include <bout/physicsmodel.hxx>
+#include <field_factory.hxx>              // Gives field factory
+#include <bout/constants.hxx>             // Gives PI and TWOPI
+// Gives own boundaries (doing so by setting ghost points)
+#include "../../common/c/include/ownBCs.hxx"
+// Give the integrators
+#include "../../common/c/include/helperFunctions.hxx"
+
+class SurfEdgeIntCyl : public PhysicsModel {
+public:
+    // Destructor
+    ~SurfEdgeIntCyl();
+protected:
+    int init(bool restarting);
+    int rhs(BoutReal t);
+private:
+    // Global variable initialization
+    // ############################################################################
+    // Variables
+    // *****************************************************************************
+    Field3D f;
+    BoutReal S, S_num;
+    BoutReal e;
+    // *****************************************************************************
+
+    // Constants
+    // *****************************************************************************
+    BoutReal Lx, Ly;    // The box dimensions
+    // *****************************************************************************
+
+    // Make a field group to communicate
+    // *****************************************************************************
+    FieldGroup com_group;
+    // *****************************************************************************
+
+    // Other objects
+    // *****************************************************************************
+    OwnBCs ownBC;           // Class containing methods which sets the ghost points
+    // *****************************************************************************
+    // ############################################################################
+};
+
+#endif
