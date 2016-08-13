a66e7b486d7c8d67f4db20be73d9aa2bc7576016
loeiten <michael.l.magnussen@gmail.com>
2016-08-13 19:52:43 +0200

    Uploaded unfinished surface integral
    Former-commit-id: ed21ef72a9d75deec527bb464c7c4b5d19f40c78

---
+++ MES/integrals/surfaceEdgeIntegral/driverSumSines.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""Method of exact solution driver"""
+
+from bout_runners.bout_runners import basic_runner
+import numpy as np
+import sys, os
+# If we add to sys.path, then it must be an absolute path
+common_dir = os.path.abspath('./../../')
+# Sys path is a list of system paths
+sys.path.append(common_dir)
+
+from common.python.postProcessingMESIntegral import perform_MES_test as postProcess
+
+# The options for the run
+# =============================================================================
+# Spatial domain (+2 adds the ghost points)
+nx = [2**n+2 for n in range(4, 5)]
+ny = [130]*len(nx)
+nz = [128]*len(nx)
+
+# Additional options
+remove_old = False
+directory  = "sumSines"
+make       = False
+nproc      = 4
+# =============================================================================
+
+
+# Create the runner
+# =============================================================================
+my_runs = basic_runner(\
+            directory  = directory ,\
+            nproc      = nproc ,\
+            # Spatial domain
+            nx         = nx,\
+            ny         = ny,\
+            nz         = nz,\
+            # Copy the source file
+            cpy_source = True  ,\
+            make       = make  ,\
+            # Sort the runs by the spatial domain
+            sort_by    = 'spatial_domain'
+            )
+# =============================================================================
+
+
+# Perform the run
+# =============================================================================
+my_runs.execute_runs(\
+                     remove_old = remove_old,\
+                     # Set the proper directory
+                     post_processing_function = postProcess,\
+                     post_process_after_every_run = False,\
+                     # Below are the kwargs arguments being passed to
+                     # the post processing function
+                     show_plot     = False,\
+                     use_dx        = True ,\
+                     use_dy        = False,\
+                     use_dz        = False,\
+                     extension     = 'pdf',\
+                    )
+# =============================================================================
