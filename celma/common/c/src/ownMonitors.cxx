a2b3846fc7a6b2c60b89bb6784bdd21650fd4f58
loeiten <michael.l.magnussen@gmail.com>
2016-08-13 19:49:57 +0200

    Updated monitors
    Former-commit-id: 98a8de9fa5e70e66002f377b1ebda848987ebe48

--- celma/common/c/src/ownMonitors.cxx
+++ celma/common/c/src/ownMonitors.cxx
@@ -16,10 +16,7 @@ void ownMonitors<PhysicsClass>::create(PhysicsClass *inputModel)
     TRACE("Halt in ownMonitors::create");
 
     model = inputModel;
-    //std::cout << inputModel->phi(0,0,0) << std::endl;
-    //std::cout << model->phi(0,0,0) << std::endl;
 }
-
 
 /*!
  * The energy monitor is a wrapper which calls functions which
@@ -27,6 +24,8 @@ void ownMonitors<PhysicsClass>::create(PhysicsClass *inputModel)
  * arguments listed.
  *
  * \note This must be defined as static in order to get the correct type
+ *       (remember that static members belong to the class rather than the
+ *       object)
  *       http://stackoverflow.com/questions/15841338/c-unresolved-overloaded-function-type
  *
  * \param[in] solver   Pointer to the solver
@@ -39,15 +38,18 @@ void ownMonitors<PhysicsClass>::create(PhysicsClass *inputModel)
 template<class PhysicsClass>
 int ownMonitors<PhysicsClass>::energyIntMon(Solver *solver, BoutReal simtime, int iter, int NOUT)
 {
-    TRACE("Halt in ownMonitors::energyIntMon");
+    TRACE("Halt in ownMonitors<PhysicsClass>::energyIntMon");
+
+    // Call the fillEnergy function
+    int lol;
+    kinEnergy(model->kinE, model->n, model->phi, model->uEPar, model->uIPar);
 
     std::cout << model->phi(0,0,0) << std::endl;
-    std::cout << "foo" << std::endl;
 //    // Call the functions
 //    // FIXME:
 //    See 6.2 for previous monitors
-//    fullE must be in the model
-//    fullEnergy(fullE, n, phi, uEPar, uIPar);
+//    kinE must be in the model
+//    fullEnergy(kinE, n, phi, uEPar, uIPar);
 //    // Calculate averages
 //    polAvgN = polAvg(n);
 //    polAvgN = polAvg(phi);
@@ -57,6 +59,37 @@ int ownMonitors<PhysicsClass>::energyIntMon(Solver *solver, BoutReal simtime, in
 //    polAvgEnergy(polAvgE, n, phi, uEPar, uIPar);
 
     return 0;
+}
+
+/*!
+ * The energy monitor is a wrapper which calls functions which
+ * calculates the monitored energy. The monitors accept only the input
+ * arguments listed.
+ *
+ * \note This must be defined as static in order for the static energyIntMon to
+ *       see it
+ *       http://stackoverflow.com/questions/15235526/the-static-keyword-and-its-various-uses-in-c
+ *
+ * ## Derivation
+ * For a single particle, we have \f$E_{kin} = \frac{1}{2}m\mathbf{v}^2\f$,
+ * whereas for a fluid, we have \f$E_{kin} = \frac{1}{2}m\int n\mathbf{u}^2 dV\f$
+ *
+ * \param[in] kinE    Variable where the kinetic energy will be stored
+ * \param[in] n       The density
+ * \param[in] phi     The potential
+ * \param[in] uEPar   The parallel electron velocity
+ * \param[in] uIPar   The parallel ion velocity
+ *
+ * \param[out] kinE   Variable where the kinetic energy is stored
+ */
+template<class PhysicsClass>
+void ownMonitors<PhysicsClass>::kinEnergy(BoutReal &kinE      ,
+                                          Field3D const &n    ,
+                                          Field3D const &phi  ,
+                                          Field3D const &uEPar,
+                                          Field3D const &uIPar)
+{
+    TRACE("Halt in ownMonitors<PhysicsClass>::kinEnergy");
 }
 
 #endif
