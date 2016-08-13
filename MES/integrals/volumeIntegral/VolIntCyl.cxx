58c6062e675b7908145a29410f27134f1e039dde
loeiten <michael.l.magnussen@gmail.com>
2016-08-13 09:01:58 +0200

    Added volume integral MES
    Former-commit-id: 74a3b12fedf1ed5e5bbef7d6dd8803e56022ce06

---
+++ MES/integrals/volumeIntegral/VolIntCyl.cxx
@@ -0,0 +1,90 @@
+// *************** Simulation of VolIntCyl *********************
+/* Geometry
+ *  x - The radial coordinate (rho)
+ *  y - The height of the cylinder (z)
+ *  z - The azimuthal coordinate (theta)
+ */
+
+#include "VolIntCyl.hxx"
+
+// Initialization of the physics
+// ############################################################################
+int VolIntCyl::init(bool restarting) {
+    TRACE("Halt in VolIntCyl::init");
+
+    // Get the option (before any sections) in the BOUT.inp file
+    Options *options = Options::getRoot();
+
+    // Load from the geometry
+    // ************************************************************************
+    Options *geom = options->getSection("geom");
+    geom->get("Lx", Lx, 0.0);
+    // ************************************************************************
+
+    // Obtain the fields
+    // ************************************************************************
+    // f
+    f = FieldFactory::get()
+        ->create3D("f:function", Options::getRoot(), mesh, CELL_CENTRE, 0);
+
+    // S
+    Options *SOpt = options->getSection("S");
+    SOpt->get("solution", S, 0.0);
+    // ************************************************************************
+
+    // Add a FieldGroup to communicate
+    // ************************************************************************
+    // Only these fields will be taken derivatives of
+    com_group.add(f);
+    // ************************************************************************
+
+    // Set boundaries manually
+    // ************************************************************************
+    f.setBoundary("f");
+    f.applyBoundary();
+    ownBC.innerRhoCylinder(f);
+    // ************************************************************************
+
+    // Communicate before taking derivatives
+    mesh->communicate(com_group);
+
+    output << "\n\n\n\n\n\n\nNow running test" << std::endl;
+
+    // Calculate the integral
+    volumeIntegral(f, S_num);
+
+    // Error in S
+    e = S_num - S;
+
+    // Save the variables
+    SAVE_ONCE2(Lx, Ly);
+    SAVE_ONCE3(e, S, S_num);
+    SAVE_ONCE (f);
+
+    // Finalize
+    dump.write();
+    dump.close();
+
+    output << "\nFinished running test, now quitting\n\n\n\n\n\n" << std::endl;
+
+    // Wait for all processors to write data
+    MPI_Barrier(BoutComm::get());
+
+    return 0;
+}
+// ############################################################################
+
+// Solving the equations
+// ############################################################################
+int VolIntCyl::rhs(BoutReal t) {
+    return 0;
+}
+// ############################################################################
+
+// Create a simple main() function
+BOUTMAIN(VolIntCyl);
+
+// Destructor
+VolIntCyl::~VolIntCyl(){
+    TRACE("VolIntCyl::~VolIntCyl");
+}
