ff3910b6db35fef355948fca52c50da10d92acb5
Michael Løiten <michael.l.magnussen@gmail.com>
2016-08-18 10:01:03 +0200

    Updated surfaceInt
    Former-commit-id: 1c167676e4863e6c1e1241bf8be10969ee317872

--- MES/integrals/surfaceEdgeIntegral/SurfEdgeIntCyl.hxx
+++ MES/integrals/surfaceEdgeIntegral/SurfEdgeIntCyl.hxx
@@ -12,6 +12,7 @@
 #include <bout/physicsmodel.hxx>
 #include <field_factory.hxx>              // Gives field factory
 #include <bout/constants.hxx>             // Gives PI and TWOPI
+#include <initialprofiles.hxx>            // Give initial_profiles
 // Gives own boundaries (doing so by setting ghost points)
 #include "../../common/c/include/ownBCs.hxx"
 // Give the integrators
@@ -19,6 +20,8 @@
 
 class SurfEdgeIntCyl : public PhysicsModel {
 public:
+    // Constructor (initialize the results vector using list initialization)
+    SurfEdgeIntCyl() : results(4, 0.0) {}
     // Destructor
     ~SurfEdgeIntCyl();
 protected:
@@ -29,14 +32,21 @@ private:
     // ############################################################################
     // Variables
     // *****************************************************************************
-    Field3D f;
-    BoutReal S, S_num;
-    BoutReal e;
+    Vector3D v;
+    BoutReal S_Xout, S_Yup, S_Ydown;
+    BoutReal S_Xout_num, S_Yup_num, S_Ydown_num;
+    BoutReal e_Xout, e_Yup, e_Ydown;
+    std::vector<BoutReal> results;
     // *****************************************************************************
 
     // Constants
     // *****************************************************************************
     BoutReal Lx, Ly;    // The box dimensions
+    // *****************************************************************************
+
+    // Switches
+    // *****************************************************************************
+    bool saveFields;
     // *****************************************************************************
 
     // Make a field group to communicate
