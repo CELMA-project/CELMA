c9a86045ceae29f8763cc65eb6d843044b5e7743
Michael Løiten <michael.l.magnussen@gmail.com>
2016-09-06 15:38:19 +0200

    Driver update
    Former-commit-id: 274e44622d990141805af792b9878263b5c03bcd

---
+++ celmaCurMom/4.0-CELMACurMomWParams/PBSDriver-b1-VienetaFlat-2-linearPhase3.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+"""Driver which runs using PBS."""
+
+from bout_runners import PBS_runner
+import numpy as np
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath('./../common/python')
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from postProcessing.plotting import combinedDriver as postProcess
+
+# The options for the run
+# =============================================================================
+# *****************************************************************************
+saveTerms           = False
+useHyperViscAzVortD = [True]
+# *****************************************************************************
+remove_old = False
+restart    = "overwrite"
+# Uncomment this if you just want to plot
+# restart      = None;
+restart_from = "b1-VienetaFlat/nout_1000_timestep_1/switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_b1-VienetaFlat-2-linearPhase2_0/"
+# Set the temporal domain
+nout       = [1000]
+timestep   = [1]
+directory  = "b1-VienetaFlat"
+# Shall we make?
+make       = False
+# =============================================================================
+
+
+# The options for the post processing function
+# =============================================================================
+xguards    = False
+yguards    = False
+xSlice     = 0
+ySlice     = 8*2
+zSlice     = 0
+tSlice     = slice(-2, None)
+showPlot   = False
+savePlot   = True
+theRunName = "b1-VienetaFlat-2-linearPhase3"
+# =============================================================================
+
+
+# The PBS options
+# =============================================================================
+# Specify the numbers used for the BOUT runs
+nproc                 = 80
+BOUT_nodes            = 4
+BOUT_ppn              = 20
+BOUT_walltime         = '72:00:00'
+BOUT_run_name         = theRunName
+post_process_nproc    = 1
+post_process_nodes    = 1
+post_process_ppn      = 20
+post_process_walltime = '0:29:00'
+post_process_queue    = 'xpresq'
+post_process_run_name = 'post' + theRunName.capitalize()
+# =============================================================================
+
+
+# Create the runner
+# =============================================================================
+myRuns = PBS_runner(\
+            directory  = directory ,\
+            nproc      = nproc ,\
+            # Set temporal domain
+            nout       = nout  ,\
+            timestep   = timestep,\
+            # Copy the source file
+            cpy_source = True  ,\
+            make       = make  ,\
+            restart    = restart,\
+            restart_from = restart_from,\
+            additional = [
+                          ('tag',theRunName,0),\
+                          ('switch'      , 'useHyperViscAzVortD',useHyperViscAzVortD),\
+                          ('switch'      , 'saveTerms'          ,saveTerms),\
+                         ],\
+            # PBS options
+            BOUT_nodes            = BOUT_nodes           ,\
+            BOUT_ppn              = BOUT_ppn             ,\
+            BOUT_walltime         = BOUT_walltime        ,\
+            BOUT_run_name         = BOUT_run_name        ,\
+            post_process_nproc    = post_process_nproc   ,\
+            post_process_nodes    = post_process_nodes   ,\
+            post_process_ppn      = post_process_ppn     ,\
+            post_process_walltime = post_process_walltime,\
+            post_process_queue    = post_process_queue   ,\
+            post_process_run_name = post_process_run_name,\
+            )
+# =============================================================================
+
+
+# Perform the run
+# =============================================================================
+myRuns.execute_runs(\
+                     remove_old               = remove_old,\
+                     post_processing_function = postProcess,\
+                     # This function will be called every time after
+                     # performing a run
+                     post_process_after_every_run = True,\
+                     # Below are the kwargs arguments being passed to
+                     # the post processing function
+                     # Switches
+                     xguards        = xguards           ,\
+                     yguards        = yguards           ,\
+                     xSlice         = xSlice            ,\
+                     ySlice         = ySlice            ,\
+                     zSlice         = zSlice            ,\
+                     tSlice         = tSlice            ,\
+                     savePlot       = savePlot          ,\
+                     saveFolderFunc = "scanWTagSaveFunc",\
+                     theRunName     = theRunName        ,\
+                    )
+# =============================================================================
