b7a7af733113d03712947dc5b045d6273148e407
Michael Løiten <michael.l.magnussen@gmail.com>
2016-09-09 08:40:51 +0200

    Fixed polAvg
    Previously polAvg returned the fluctuations, now it returns the poloidal
    average. I.e. it works as an operator.
    
    
    Former-commit-id: f9ca63111644832145ce335df8e0133947e1ad16

--- celmaApar/common/CELMAPython/plotting/drivers1D.py
+++ celmaApar/common/CELMAPython/plotting/drivers1D.py
@@ -319,7 +319,7 @@ def single1DDriver(path                      ,\
                    ySlice     = slice(0,None),\
                    zSlice     = slice(0,None),\
                    tSlice     = None         ,\
-                   polAvg     = False        ,\
+                   subPolAvg  = False        ,\
                    showPlot   = False        ,\
                    savePlot   = True         ,\
                    saveFolder = None         ,\
@@ -339,8 +339,8 @@ def single1DDriver(path                      ,\
     ySlice     - How to slice in y
     zSlice     - How to slice in z
     tSlice     - How to slice in t
-    polAvg     - Whether or not to perform a poloidal average of
-                 the data
+    subPolAvg  - Whether or not the poloidal average should be
+                 subtracted from the data
     showPlot   - If the plot is to be displayed
     savePlot   - If the plot is to be saved
     saveFolder - Name of save folder
@@ -363,7 +363,7 @@ def single1DDriver(path                      ,\
                      ySlice     = ySlice    ,\
                      zSlice     = zSlice    ,\
                      tSlice     = tSlice    ,\
-                     polAvg     = polAvg    ,\
+                     subPolAvg  = subPolAvg ,\
                      showPlot   = showPlot  ,\
                      savePlot   = savePlot  ,\
                      saveFolder = saveFolder,\
