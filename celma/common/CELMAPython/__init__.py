ad0b74cbca084fb38c4769d39937ebb3c9da355c
Michael Løiten <michael.l.magnussen@gmail.com>
2016-09-11 18:41:25 +0200

    Cleaned up API in post-processing
    Former-commit-id: b057964b839070059be6c32ee38f8f7e7275141b

--- celma/common/CELMAPython/__init__.py
+++ celma/common/CELMAPython/__init__.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
 
+""" Init for the CELMAPython package"""
+
+import matplotlib.pyplot as plt
+
+# Set the plot style for all plots
+titleSize = 30
+plt.rc("font",   size      = 30)
+plt.rc("axes",   labelsize = 25, titlesize = titleSize)
+plt.rc("xtick",  labelsize = 25)
+plt.rc("ytick",  labelsize = 25)
+plt.rc("legend", fontsize  = 20)
+plt.rc("lines",  linewidth = 2)
+
+# Set proper backend
+try:
+    plt.figure(0)
+except RuntimeError:
+    plt.switch_backend('Agg')
+    plt.figure(0)
+plt.close(0)
