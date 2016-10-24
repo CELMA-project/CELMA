489a78541e9bd0cb68d8d87baa4a1b7fc73473ed
Michael Løiten <michael.l.magnussen@gmail.com>
2016-10-24 15:21:15 +0200

    Typo fix
    Former-commit-id: 6c3bfaf68ba4603bf0c3663f9438ea55fe535944

--- celmaCurMom/4.1-CELMACurMomWParamsConstVisc/PBSDriver-a1-KiwiFlat-scanB0.py
+++ celmaCurMom/4.1-CELMACurMomWParamsConstVisc/PBSDriver-a1-KiwiFlat-scanB0.py
@@ -97,7 +97,7 @@ justPostProcess = True
 postProcessInit = False
 postProcessExp  = False
 postProcessLin  = False
-postProcessTrub = False
+postProcessTurb = False
 # Extra post-processors
 postProcessLinProfiles     = False
 postProcessTurbProfiles    = False
@@ -490,7 +490,7 @@ if postProcessLinProfiles:
 #}}}
 
 #{{{Turbulence runner
-if postProcessTrub:
+if postProcessTurb:
     curPostProcessor = postBoutRunner
 else:
     curPostProcessor = None
