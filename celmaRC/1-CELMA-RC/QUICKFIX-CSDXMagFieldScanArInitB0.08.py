b54f2c54ae593a0ed57865edabfe5fe0c4d28b82
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-05 14:21:23 +0100

    Conclusion and driver update
    Former-commit-id: d3183ea966b4eece82edd0c3515ef2e9921d974d

--- celmaRC/1-CELMA-RC/QUICKFIX-CSDXMagFieldScanArInitB0.08.py
+++ celmaRC/1-CELMA-RC/QUICKFIX-CSDXMagFieldScanArInitB0.08.py
@@ -15,7 +15,7 @@ B0 = (  8.0e-2,)
 Lx = (  6.2906,)
 Ly = (220.1715,)
 # *****************************************************************************
-nz = 1
+nz           = 1
 restart      = "append"
 restart_from = "CSDXMagFieldScanAr/nout_2_timestep_2000.0/nz_1/geom_Lx_6.2906_geom_Ly_220.1715_input_B0_0.08_ownFilters_type_none_switch_useHyperViscAzVortD_False_tag_CSDXMagFieldScanAr-0-initialize_0/"
 timestep     = (2e3,)
