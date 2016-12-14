94108da0d902ae3b542b7ba33039bca837f96124
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-14 18:30:21 +0100

    Start of flux
    Former-commit-id: 6bf4349c737e27e00b022b1a0e432a8d6e1f5408

---
+++ celmaRC/common/CELMAPython/radialFlux/driverRadialFlux.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+"""
+Contains drivers for the PSD
+"""
+
+from .statsAndSignalsDriver import StatsAndSignalsDrivers
+from ..statsAndSignals import collectEnergy, PlotEnergy
+
+#{{{DriverPSD
+class DriverPSD(PointsSuperClass):
+    """
+    Class which handles the PSD data.
+    """
+
+    #{{{Constructor
+    def __init__(self,\
+                 *args,\
+                 **kwargs):
+        #{{{docstring
+        """
+        This constructor:
+
+        1. Calls the parent constructor
+        2. Sets pltSize
+
+        Parameters
+        ----------
+        *args : positional arguments
+            See the parent constructor for details.
+        **kwargs : keyword arguments
+            See the parent constructor for details.
+        """
+        #}}}
+
+        # Call the constructor of the parent class
+        super().__init__(*args, **kwargs)
+
+        # Set member data
+        self._pltSize = (12, 9)
+
+        # Placeholder for the timeTrace
+        self._PSD = None
+    #}}}
+
+    #{{{getPSD
+    def getPSD(self):
+        """Obtain the PSD"""
+        # Create the probes
+        self._PSD = calcPSD(self._paths,\
+                            self._varName,\
+                            self._xInd,\
+                            self._yInd,\
+                            self._zInd,\
+                            converToPhysical = self._convertToPhysical,\
+                            mode             = self._mode,\
+                            tSlice           = self._tSlice,\
+                            )
+    #}}}
+
+    #{{{plotPSD
+    def plotPSD(self):
+        """Plots the PSD"""
+
+        # Calculate the probes if not already done
+        if self._timeTrace == None:
+            self.getTimeTraces()
+
+        # Create the energyPlotter
+        PSDPlotter = PlotPSD(\
+                self._paths                                ,\
+                self._PSD                                  ,\
+                convertToPhysical = self._convertToPhysical,\
+                showPlot          = self._showPlot         ,\
+                savePlot          = self._savePlot         ,\
+                extension         = self._extension        ,\
+                savePath          = self._savePath         ,\
+                pltSize           = self._pltSize          ,\
+                                  )
+
+        PSDPlotter.plotPSD()
+    #}}}
+#}}}
---
+++ celmaRC/common/CELMAPython/radialFlux/plotRadialFlux.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+"""Class for timeTrace plot"""
+
+from ..plotHelpers import plotNumberFormatter, seqCMap2, seqCMap3
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib.gridspec import GridSpec
+import numpy as np
+import os
+
+#{{{PlotPDF
+class PlotPDF(PlotsSuperClass):
+    """
+    Class which contains the PDF data and the plotting configuration.
+    """
+
+    #{{{__init___
+    def __init__(self    ,\
+                 *args   ,\
+                 PDF     ,\
+                 mode    ,\
+                 **kwargs,\
+                 ):
+        #{{{docstring
+        """
+        This constructor:
+
+        1. Calls the parent constructor
+        2. Sets the member data
+        3. Prepares the labels
+
+        Parameters
+        ----------
+        PDF : dict
+            Dictionary where the keys are on the form "rho,theta,z".
+            The value is a dict containing of
+            {"pdfX":pdfX, "pdfY":"pdfY"}
+        mode : ["normal"|"fluct"]
+            What mode the input is given in.
+        """
+        #}}}
+
+        # Call the constructor of the parent class
+        super().__init__(*args, **kwargs)
+
+        # Set the member data
+        self._PDF    = PDF
+        self._colors = seqCMap3(np.linspace(0, 1, len(timeTraces.keys())))
+
+        # Obtain the varname
+        ind  = PDF.keys()[0]
+        keys = PDF[ind].keys()
+        self._varName = [var[:-4] for var in keys if "PDF" in var][0]
+
+        # Set the labels
+        # NOTE: The probability does not have any units, but in order to
+        #       obtain the probability one has to integrate over
+        #       the PDF.
+        #       Thus will the PDF have the dimension of the inverse of
+        #       what is on the x axis
+        pltVarName   = self.ph.getVarPltName(self._varname)
+
+        norm  = self.uc.conversionDict[self._varName]["normalization"]
+        units = self.uc.conversionDict[self._varName]["units"]
+
+        # Set the variable label
+        if probes.helper.convertToPhysical:
+            if mode == "normal":
+                self._xLabel = r"${}$ $[{}]$"
+            elif mode == "fluct"
+                self._xLabel = r"$\tilde{{{}}}$ $[{}]$"
+            self._xLabel = self._xLabel.format(pltVarName, Units)
+            self._yLabel = r"$\mathrm{{PDF}}(\tilde{{{}}})$".\
+                    format(pltVarName))
+        else:
+            if mode == "normal":
+                self._xLabel = r"${}{}$"
+            elif mode == "fluct"
+                self._xLabel = r"$\tilde{{{}}}{}$"
+            self._xLabel = self._xLabel.format(pltVarName, norm)
+            self._yLabel = r"$\mathrm{{PDF}}(\tilde{{{}}}{})$".\
+                    format(pltVarName, norm)
+    #}}}
+
+    #{{{plotPDFs
+    def plotPDFs(self):
+        """ Plots the time traces."""
+
+        # Create the plot
+        fig = plt.figure(figsize = self._pltSize)
+        ax  = fig.add_subplot(111)
+
+        keys = sort(self._timeTraces.keys())
+
+        for key, color in keys, self._colors:
+            # Make the label
+            rhoInd, thetaInd, zInd = key.split(",")
+            label = (r"$\rho={}$ $\theta={}$ $z={}$").format(rho, theta. z)
+
+            ax.plot(self._PDF[key]["{}PDFX"].self._varName,\
+                    self._PDF[key]["{}PDFY"].self._varName,\
+                    color=color,\
+                    label=label)
+
+        # Set axis labels
+        ax.set_xlabel(self._xLabel)
+        ax.set_ylabel(self._yLabel)
+
+        # Make the plot look nice
+        self.ph.makePlotPretty(ax, rotation = 45)
+
+        if self._showPlot:
+            plt.show()
+
+        if self._savePlot:
+            fileName = "{}.{}".\
+                format(os.path.join(self._savePath, "PDF"),\
+                       self._extension)
+            self.ph.savePlot(fig, fileName, (self._leg,))
+
+        plt.close(fig)
+    #}}}
+#}}}
