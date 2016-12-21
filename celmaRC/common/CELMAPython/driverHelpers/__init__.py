f7e05ecbc7f1cfd0a944aa8afe526b869415c9ab
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-21 11:36:39 +0100

    Checkpoint after 1D drivers
    Former-commit-id: 5dfb22ef66a735fdce82cd15dbf99e176d1153f0

---
+++ celmaRC/common/CELMAPython/driverHelpers/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""
+Init-file for driverHelpers
+"""
+
+from .convertToCurrentScanParameters import convertToCurrentScanParameters
+from .saveFolderFuncs import scanWTagSaveFunc
