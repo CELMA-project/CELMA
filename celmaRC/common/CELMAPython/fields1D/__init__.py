28835e8929df013aae8e8726fd90a0a075a3b2ef
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-21 11:36:39 +0100

    Checkpoint after 1D drivers
    Former-commit-id: 350fb5f3affc2f049f288b41e29da34415330d78

--- celmaRC/common/CELMAPython/fields1D/__init__.py
+++ celmaRC/common/CELMAPython/fields1D/__init__.py
@@ -3,4 +3,5 @@
 """ Init-file for the fields 1D """
 
 from .collectAndCalcFields1D import CollectAndCalcFields1D
+from .driverFields1D import driver1DFieldSingle, Driver1DFields
 from .plotFields1D import PlotAnim1DRadial, PlotAnim1DParallel
