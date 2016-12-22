63e412d1019f97dea17b52da17dff0ba3a6702ec
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-22 16:51:14 +0100

    Bugfixes, checkpoint before regex sed
    Former-commit-id: 70df0b2b068091d3c4d0de4bfc75090e6b464755

--- celmaRC/1-CELMA-RC/test2DField.py
+++ celmaRC/1-CELMA-RC/test2DField.py
@@ -8,7 +8,11 @@ commonDir = os.path.abspath("./../common")
 # Sys path is a list of system paths
 sys.path.append(commonDir)
 
-from CELMAPy.fields2D import driver2DFieldSingle
+from CELMAPy.fields2D import (driver2DFieldPerpSingle,\
+                              driver2DFieldParSingle,\
+                              driver2DFieldPolSingle,\
+                              driver2DFieldPerpParSingle,\
+                             )
 
 #{{{singlePerpTest
 def singlePerpTest():
@@ -21,34 +25,157 @@ def singlePerpTest():
         "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/"\
        )
 
-    savePath = "."
-    varName = "lnN"
+    savePath          = "."
+    varName           = "lnN"
     convertToPhysical = True
-    xSlice = None
-    ySlice = 16
-    zSlice = None
-    tSlice = None
-    fluct  = False
-    mode   = "perp"
+    xSlice            = None
+    ySlice            = 16
+    zSlice            = None
+    tSlice            = None
+    fluct             = False
+    varyMaxMin        = False
 
     print("\n\nTesting perp 2D field")
-    driver2DFieldSingle(collectPaths     ,\
-                        savePath         ,\
-                        varName          ,\
-                        convertToPhysical,\
-                        xSlice           ,\
-                        ySlice           ,\
-                        zSlice           ,\
-                        tSlice           ,\
-                        fluct            ,\
-                        mode             ,\
-                        xguards  = False ,\
-                        yguards  = False ,\
-                        showPlot = False ,\
-                        savePlot = True  ,\
-                                        )
+    driver2DFieldPerpSingle(collectPaths     ,\
+                            savePath         ,\
+                            varName          ,\
+                            convertToPhysical,\
+                            xSlice           ,\
+                            ySlice           ,\
+                            zSlice           ,\
+                            tSlice           ,\
+                            fluct            ,\
+                            varyMaxMin       ,\
+                            xguards  = False ,\
+                            yguards  = False ,\
+                            showPlot = False ,\
+                            savePlot = True  ,\
+                                            )
+    print("Success!\n\n")
+#}}}
+
+#{{{singleParTest
+def singleParTest():
+    """
+    Runs the single par test for field2D
+    """
+
+    collectPaths =\
+        (\
+        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/",\
+        )
+
+    savePath          = "."
+    varName           = "jPar"
+    convertToPhysical = True
+    xSlice            = None
+    ySlice            = None
+    zSlice            = 0
+    tSlice            = None
+    fluct             = True
+    varyMaxMin        = False
+
+    print("\n\nTesting par 2D field")
+    driver2DFieldParSingle(collectPaths     ,\
+                            savePath         ,\
+                            varName          ,\
+                            convertToPhysical,\
+                            xSlice           ,\
+                            ySlice           ,\
+                            zSlice           ,\
+                            tSlice           ,\
+                            fluct            ,\
+                            varyMaxMin       ,\
+                            xguards  = False ,\
+                            yguards  = False ,\
+                            showPlot = False ,\
+                            savePlot = True  ,\
+                                            )
+    print("Success!\n\n")
+#}}}
+
+#{{{singlePolTest
+def singlePolTest():
+    """
+    Runs the single pol test for field2D
+    """
+
+    collectPaths =\
+        (\
+        "CSDXMagFieldScanAr/nout_2_timestep_50/nz_256/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_ownFilters_type_none_switch_useHyperViscAzVortD_False_tag_CSDXMagFieldScanAr-1-expand_0/",\
+        )
+
+    savePath          = "."
+    varName           = "uEPar"
+    convertToPhysical = True
+    xSlice            = 16
+    ySlice            = None
+    zSlice            = None
+    tSlice            = None
+    fluct             = False
+    varyMaxMin        = False
+
+    print("\n\nTesting pol 2D field")
+    driver2DFieldPolSingle(collectPaths      ,\
+                            savePath         ,\
+                            varName          ,\
+                            convertToPhysical,\
+                            xSlice           ,\
+                            ySlice           ,\
+                            zSlice           ,\
+                            tSlice           ,\
+                            fluct            ,\
+                            varyMaxMin       ,\
+                            xguards  = False ,\
+                            yguards  = False ,\
+                            showPlot = False ,\
+                            savePlot = True  ,\
+                                             )
+    print("Success!\n\n")
+#}}}
+
+#{{{singlePerpParTest
+def singlePerpParTest():
+    """
+    Runs the single perpPar test for field2D
+    """
+    collectPaths =\
+        (\
+        "CSDXMagFieldScanAr/nout_2_timestep_50/nz_256/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_ownFilters_type_none_switch_useHyperViscAzVortD_False_tag_CSDXMagFieldScanAr-1-expand_0/",\
+       )
+
+    savePath          = "."
+    varName           = "uIPar"
+    convertToPhysical = True
+    xSlice            = None
+    ySlice            = None
+    zSlice            = None
+    yInd              = 50
+    zInd              = 1
+    tSlice            = None
+    fluct             = False
+    varyMaxMin        = False
+
+    print("\n\nTesting perpPar 2D field")
+    driver2DFieldPerpParSingle(collectPaths     ,\
+                               savePath         ,\
+                               varName          ,\
+                               convertToPhysical,\
+                               xSlice           ,\
+                               ySlice           ,\
+                               zSlice           ,\
+                               yInd             ,\
+                               zInd             ,\
+                               tSlice           ,\
+                               fluct            ,\
+                               varyMaxMin       ,\
+                               xguards  = False ,\
+                               yguards  = False ,\
+                               showPlot = False ,\
+                               savePlot = True  ,\
+                                            )
     print("Success!\n\n")
 #}}}
 
 if __name__ == "__main__":
-    singlePerpTest()
+    singlePerpParTest()
--- celmaRC/common/CELMAPy/fields2D/__init__.py
+++ celmaRC/common/CELMAPy/fields2D/__init__.py
@@ -3,7 +3,11 @@
 """ Init-file for the fields 2D """
 
 from .collectAndCalcFields2D import CollectAndCalcFields2D
-from .driverFields2D import driver2DFieldSingle
+from .driverFields2D import (driver2DFieldPerpSingle,\
+                             driver2DFieldParSingle,\
+                             driver2DFieldPolSingle,\
+                             driver2DFieldPerpParSingle,\
+                            )
 from .plotFields2D import (PlotAnim2DPerp,\
                            PlotAnim2DPar,\
                            PlotAnim2DPol,\
