3ff54e1c99c3c6cd161116ddc9bd14a55bacae00
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-23 23:19:11 +0100

    Field2D working again :D
    Former-commit-id: f775529c10548aee06563643ff22e1fd24aa55df

--- celmaRC/common/CELMAPy/superClasses/driverPlotFieldsSuperClass.py
+++ celmaRC/common/CELMAPy/superClasses/driverPlotFieldsSuperClass.py
@@ -4,38 +4,59 @@
 Contains the super class driver for fields 1D and fields 2D
 """
 
-from .collectAndCalcSuperClass import CollectAndCalcSuperClass
-from .plotSuperClass import PlotSuperClass
+from .driverSuperClass import DriverSuperClass
 
 #{{{DriverPlotFieldsSuperClass
-class DriverPlotFieldsSuperClass(CollectAndCalcSuperClass):
+class DriverPlotFieldsSuperClass(DriverSuperClass):
     """
     The parent driver of 1D and 2D field plotting
     """
 
     #{{{Constructor
-    def __init__(self                 ,\
-                 *args                ,\
-                 xguards       = False,\
-                 yguards       = False,\
-                 xSlice        = None ,\
-                 ySlice        = None ,\
-                 zSlice        = None ,\
-                 tSlice        = None ,\
-                 xInd          = None ,\
-                 yInd          = None ,\
-                 zInd          = None ,\
+    def __init__(self                    ,\
+                 *args                   ,\
+                 convertToPhysical = True,\
                  **kwargs):
         #{{{docstring
         """
         This constructor:
             * Calls the parent class
-            * Sets the common memberdata
 
         Parameters
         ----------
-        **args : positional arguments
+        *args : positional arguments
             See parent class for details.
+        convertToPhysical : bool
+            Whether or not to convert to physical units.
+        **kwargs : keyword arguments
+            See parent class for details.
+        """
+        #}}}
+
+        # Call the constructors of the parent class
+        super().__init__(*args, **kwargs)
+
+        # Set member data
+        self.convertToPhysical = convertToPhysical
+    #}}}
+
+    #{{{setGuardSlicesAndIndices
+    def setGuardSlicesAndIndices(self           ,\
+                                 xguards = False,\
+                                 yguards = False,\
+                                 xSlice  = None ,\
+                                 ySlice  = None ,\
+                                 zSlice  = None ,\
+                                 tSlice  = None ,\
+                                 xInd    = None ,\
+                                 yInd    = None ,\
+                                 zInd    = None ):
+        #{{{docstring
+        """
+        Sets the guard, the slices and the indices
+
+        Parameters
+        ----------
         xguards : bool
             If xguards should be included when collecting.
         yguards : bool
@@ -54,23 +75,18 @@ class DriverPlotFieldsSuperClass(CollectAndCalcSuperClass):
             Fixed z index.
         zInd : int
             Fixed theta index.
-        **kwargs : keyword arguments
-            See parent class for details.
         """
         #}}}
 
-        # Call the constructors of the parent class
-        super().__init__(*args, **kwargs)
-
         # Set the member data
-        self._xguards       = xguards
-        self._yguards       = yguards
-        self._xInd          = xInd
-        self._yInd          = yInd
-        self._zInd          = zInd
-        self._useSubProcess = useSubProcess
-
-        # Set the slices
-        setSlice(xSlice, ySlice, zSlice, tSlice)
-    #}}}
+        self._xguards = xguards
+        self._yguards = yguards
+        self._xSlice  = xSlice
+        self._ySlice  = ySlice
+        self._zSlice  = zSlice
+        self._tSlice  = tSlice
+        self._xInd    = xInd
+        self._yInd    = yInd
+        self._zInd    = zInd
+    #}}}}
 #}}}
