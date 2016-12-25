6cf2d92c5a1c30f664118421e1527e87349dd761
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-25 15:02:26 +0100

    Fixing time trace checkpoint
    Former-commit-id: 48dbfdab1f2a8d25df13220ee8bdad2e8cc5993c

--- celmaRC/1-CELMA-RC/testTimeTraces.py
+++ celmaRC/1-CELMA-RC/testTimeTraces.py
@@ -8,7 +8,64 @@ commonDir = os.path.abspath("./../common")
 # Sys path is a list of system paths
 sys.path.append(commonDir)
 
-from CELMAPy.timeTrace import DriverTimeTrace
+from CELMAPy.timeTrace import driverTimeTrace
+
+#{{{timeTraceTest
+def timeTraceTest():
+    """
+    Runs the single perp test for the time traces
+    """
+
+    collectPaths =\
+       (\
+        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
+        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/"\
+       )
+
+    varName           = "lnN"
+    convertToPhysical = True
+    mode              = "fluct"
+
+    xInd              = 16
+    yInd              = 16
+    zInd              = 128
+    tSlice            = None
+    nPoints           = 3
+    equallySpace      = None
+    steadyStatePath   = None
+
+    indicesArgs   = (xInd, yInd, zInd)
+    indicesKwargs = {"tSlice"          : tSlice         ,\
+                     "nPoints"         : nPoints        ,\
+                     "equallySpace"    : equallySpace   ,\
+                     "steadyStatePath" : steadyStatePath,\
+                     }
+
+    savePath          = "."
+
+    plotSuperKwargs = {\
+                        "showPlot"     : False,\
+                        "savePlot"     : True,\
+                        "savePath"     : savePath,\
+                        "savePathFunc" : None,\
+                        "extension"    : None,\
+                        "dmp_folders"  : None,\
+                       }
+
+    print("\n\nTesting single time trace")
+    driverTimeTrace(collectPaths     ,\
+                    varName          ,\
+                    convertToPhysical,\
+                    mode             ,\
+                    indicesArgs      ,\
+                    indicesKwargs    ,\
+                    plotSuperKwargs  ,\
+                   )
+
+    print("Success!\n\n")
+#}}}
+
+
 
 #{{{driverTest
 def driverTest():
