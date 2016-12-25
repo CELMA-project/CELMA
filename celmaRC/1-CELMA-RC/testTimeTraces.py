8b8dce3593527cc0e464367997ac0b1122553899
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-25 23:52:01 +0100

    Finished time traces class
    Former-commit-id: 146053b79271f659e7918f524afb500d0da6c6e2

--- celmaRC/1-CELMA-RC/testTimeTraces.py
+++ celmaRC/1-CELMA-RC/testTimeTraces.py
@@ -8,12 +8,12 @@ commonDir = os.path.abspath("./../common")
 # Sys path is a list of system paths
 sys.path.append(commonDir)
 
-from CELMAPy.timeTrace import driverTimeTrace
+from CELMAPy.timeTrace import DriverTimeTrace, driverTimeTrace
 
 #{{{timeTraceTest
 def timeTraceTest():
     """
-    Runs the single perp test for the time traces
+    Runs the test for the time traces
     """
 
     collectPaths =\
@@ -65,63 +65,67 @@ def timeTraceTest():
     print("Success!\n\n")
 #}}}
 
-
-
 #{{{driverTest
 def driverTest():
     """
-    Runs the driver test for the time traces
+    Runs the test for the time traces
     """
-    dmp_folders  = (
-        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0",
-            )
-    collectPaths =\
-        (\
-        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0",
-        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1",
-        )
 
-# FIXME: Try n
-    varName           = "lnN"
+    dmp_folders  = ("CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1",)
+    collectPaths =\
+       (\
+        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
+        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/"\
+       )
+
+    useSubProcess = False
+
+    varName           = "n"
     convertToPhysical = True
-    savePathFunc    = "scanWTagSaveFunc"
-    useSubProcess     = False
-    theRunName        = "test"
     mode              = "fluct"
-    nPoints           = 5
+
     xInd              = None
     yInd              = 16
-    zInd              = 0
+    zInd              = 128
     tSlice            = None
+    nPoints           = 3
+    equallySpace      = "x"
 
-    equallySpace = "x"
     steadyStatePath   = "CSDXMagFieldScanAr/nout_2_timestep_50/nz_256/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_ownFilters_type_none_switch_useHyperViscAzVortD_False_tag_CSDXMagFieldScanAr-1-expand_0/"
 
+    indicesArgs   = (xInd, yInd, zInd)
+    indicesKwargs = {"tSlice"          : tSlice         ,\
+                     "nPoints"         : nPoints        ,\
+                     "equallySpace"    : equallySpace   ,\
+                     "steadyStatePath" : steadyStatePath,\
+                     }
+
+    plotSuperKwargs = {\
+                        "showPlot"     : False,\
+                        "savePlot"     : True,\
+                        "savePath"     : None,\
+                        "savePathFunc" : None,\
+                        "extension"    : None,\
+                        "dmp_folders"  : None,\
+                       }
+
     print("\n\nTesting time trace driver")
-    dtt = DriverTimeTrace(
-                # DriverPostProcessingSuperClass
-                dmp_folders                          ,\
-                collectPaths      = collectPaths     ,\
-                convertToPhysical = convertToPhysical,\
-                showPlot          = False            ,\
-                savePlot          = True             ,\
-                saveFolder        = None             ,\
-                savePathFunc      = savePathFunc     ,\
-                useSubProcess     = useSubProcess    ,\
-                extension         = "png"            ,\
-                # scanWTagSaveFunc
-                theRunName = theRunName,\
-                # CollectAndCalcPointsSuperClass
-                mode            = mode           ,\
-                nPoints         = nPoints        ,\
-            )
-    dtt.setVarName(varName)
-    dtt.setIndices(xInd, yInd, zInd,\
-                   tSlice = tSlice, equallySpace = equallySpace,\
-                   steadyStatePath = steadyStatePath)
-    import pdb; pdb.set_trace()
-    dtt.plotTimeTrace()
-    print("\n\nSuccess!")
+    dTT = DriverTimeTrace(
+                     # DriverTimeTraces
+                     dmp_folders                ,\
+                     indicesArgs                ,\
+                     indicesKwargs              ,\
+                     plotSuperKwargs            ,\
+                     varName           = "n"    ,\
+                     mode              = "fluct",\
+                     # DriverPointsSuperClass
+                     convertToPhysical = convertToPhysical,\
+                     # DriverSuperClass
+                     collectPaths  = collectPaths ,\
+                     useSubProcess = useSubProcess,\
+                          )
+    dTT.driverTimeTrace()
+    print("Success!\n\n")
 #}}}
 
 if __name__ == "__main__":
--- celmaRC/common/CELMAPy/superClasses/__init__.py
+++ celmaRC/common/CELMAPy/superClasses/__init__.py
@@ -7,6 +7,7 @@ Init-file for superClasses
 from .collectAndCalcFieldsSuperClass import CollectAndCalcFieldsSuperClass
 from .collectAndCalcPointsSuperClass import CollectAndCalcPointsSuperClass
 from .driverPlotFieldsSuperClass import DriverPlotFieldsSuperClass
+from .driverPointsSuperClass import DriverPointsSuperClass
 from .collectAndCalcSuperClass import CollectAndCalcSuperClass
 from .plotSuperClass import PlotSuperClass
 from .plotAnimSuperClasses import (PlotAnimSuperClass,\
