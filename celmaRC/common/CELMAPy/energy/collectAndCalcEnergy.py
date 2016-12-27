ed48cb51b25f474a93587dcf11e7636f84aae96d
Michael Løiten <michael.l.magnussen@gmail.com>
2016-12-27 01:46:48 +0100

    Checkpoint energies
    Former-commit-id: 9bb89340c80bdbf00c57a1873f0878e11784a571

--- celmaRC/common/CELMAPy/energy/collectAndCalcEnergy.py
+++ celmaRC/common/CELMAPy/energy/collectAndCalcEnergy.py
@@ -146,30 +146,21 @@ def calcEnergiesWPy():
     # Delete objects not needed to avoid unnecessary memory consumption
     del variables, lnN
 
-    polAvgPerpKinEE, polAvgPerpKinEI, fluctPerpKinEE, fluctPerpKinEI = calcPerpEnergiesWPy(n)
+    nPolAvg = polAvg(n)
+    nFluct  = n - nPolAvg
 
-YOU ARE HERE
-def calcPerpEnergiesWPy(n):
-    # Collect
-    varStrings = ("phi",)
-    variables  = collectiveCollect(paths, varStrings)
-    phi        = variables["phi"]
+    # Delete objects not needed to avoid unnecessary memory consumption
+    del n
 
+    polAvgPerpKinEE, polAvgPerpKinEI, fluctPerpKinEE, fluctPerpKinEI =\
+            calcPerpEnergiesWPy(n)
 
-                 "polAvgPerpKinEE",\
-                 "polAvgPerpKinEI",\
-                 "fluctPerpKinEE" ,\
-                 "fluctPerpKinEI" ,\
-
-
-
-
-
+    # FIXME: Remember to collect time
+    YOU ARE HERE:  Parallel energies
 
     momDensPar = variables["momDensPar"]
     jPar       = variables["jPar"]
 
-    # Delete the dictionary to avoid unnecessary memory consumption
                   ,\
                   "jPar",\
 
@@ -185,3 +176,133 @@ def calcPerpEnergiesWPy(n):
 
                  "t_array"        ,\
     clean with del big arrays
+
+
+
+
+
+# FIXME: Add docstring (maybe just write it directly into the thesis)
+# FIXME: Input nPolAvg and nFluct
+#{{{calcPerpEnergiesWPy
+def calcPerpEnergiesWPy(n):
+    # Collect
+    varStrings = ("phi",)
+    variables  = collectiveCollect(paths, varStrings)
+    phi        = variables["phi"]
+    phiPolAvg  = polAvg(phi)
+    phiFluct   = phi - phiPolAvg
+
+    # Delete the dictionary and phi to avoid unnecessary memory consumption
+    del variables, phi
+
+    uPerpPolAvgSquared =\
+            calcGradPerpADotGradPerpB(phiPolAvg, phiPolAvg)
+
+    uPerpFluctUPerpPolAvg =\
+            calcGradPerpADotGradPerpB(phiFluct, phiFluct)
+
+    uPerpFluctSquared =\
+            calcGradPerpADotGradPerpB(phiFluct, phiFluct)
+
+    nPolAvguPerpFluctSquaredPolAvg = polAvg(nPolAvg*uPerpFluctSquared)
+    nFluctuPerpFluctUPerpPolAvgTimesTwoPolAvg =\
+            polAvg(nFluctuPerpFluctUPerpPolAvgTimesTwoPolAvg)
+
+    EPerpFluctIntegrand =\
+            nFluct*uPerpPolAvgSquared +\
+            2.0*nPolAvg*uPerpFluctUPerpPolAvg +\
+            (nPolAvg*uPerpFluctSquared - nPolAvguPerpFluctSquaredPolAvg) +\
+            (2.0*nFluct*uPerpFluctUPerpPolAvg -\
+                    nFluctuPerpFluctUPerpPolAvgTimesTwoPolAvg)
+
+    # Delete variables to avoid unnecessary memory consumption
+    del uPerpFluctSquared, uPerpFluctUPerpPolAvg
+
+    # NOTE: This is not the energy per unit volume as we have not taken
+    #       the poloidal average of nFluct*uPerpFluctSquared.
+    # NOTE: int <a> d theta = int a d theta
+    EPerpPolAvgIntegrand =\
+            nPolAvg*uPerpPolAvgSquared +\
+            nFluct*uPerpFluctSquared +\
+            nPolAvguPerpFluctSquaredPolAvg +\
+            nFluctuPerpFluctUPerpPolAvgTimesTwoPolAvg
+
+    EPerpFluct  = volInt(EPerpFluctIntegrand)
+    EPerpPolAvg = volInt(EPerpPolAvgIntegrand)
+
+    # Delete variables to avoid unnecessary memory consumption
+    del EPerpFluctIntegrand, EPerpPolAvgIntegrand
+
+    # NOTE: All collected variables are normalized, so the normalized
+    #       energy is the output
+# FIXME: Collect mu
+    polAvgPerpKinEE = (1.0/mu)*EPerpPolAvg
+    polAvgPerpKinEI = EPerpPolAvg
+    fluctPerpKinEE  = (1.0/mu)*EPerpFluct
+    fluctPerpKinEI  = EPerpFluct
+
+    return polAvgPerpKinEE, polAvgPerpKinEI, fluctPerpKinEE, fluctPerpKinEI
+#}}}
+
+# FIXME
+invRhoSquared = (1/dh.rho)**(2.0)
+#{{{calcGradPerpADotGradPerpB
+def calcGradPerpADotGradPerpB(a,b):
+    #{{{docstring
+    r"""
+    Returns \grad_\perp a \cdot \grad_\perp b
+
+    Parameters
+    ----------
+    a : array-like
+        The a in the expression above.
+    b : array-like
+        The b in the expression above.
+
+    Returns
+    -------
+    gradPerpADotGradPerpB
+    """
+    #}}}
+    return DDX(a)*DDX(b) + invRhoSquared*DDZ(a)*DDZ(b)
+#}}}
+
+# FIXME: Move to collectAndCalcHelpers
+#{{{volInt
+def volInt(f, dh):
+    #{{{docstring
+    """
+    Returns the volume integrated of a field.
+
+    The integration uses the rectangle rule in 3D, and assumes that the
+    point is in the center of a volume cell.
+    A simple, stupid, safe approach has been used
+
+    Parameters
+    ----------
+    f : array 4d
+       The variable to volume integrate
+
+    Returns
+    -------
+    result : array 1d
+       The time trace of the volume integrated variable.
+    """
+    #}}}
+    # Initialize the result
+    result = 0.0
+
+    _, nx, ny, nz = f.shape
+    for xInd in range(nx):
+        for yInd in range(ny):
+            for zInd in range(nz):
+                # NOTE: J = dh.rho
+                result +=
+                    f       [:, xInd, yInd, zInd] *\
+                    dh.rho  [   xInd, yInd]       *\
+                    dh.dx   [   xInd, yInd]       *\
+                    dh.dy   [   xInd, yInd]       *\
+                    dh.dz
+
+    return result
+#}}}
