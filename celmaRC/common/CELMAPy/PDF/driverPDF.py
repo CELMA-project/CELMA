00a628c35411d08fee82ab78b4a829fba5f82fc7
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-02 22:46:32 +0100

    Fixed PDF
    Former-commit-id: 36727cfce3c9bafef0c261858dee99aed06ebb1e

--- celmaRC/common/CELMAPy/PDF/driverPDF.py
+++ celmaRC/common/CELMAPy/PDF/driverPDF.py
@@ -1,85 +1,155 @@
 #!/usr/bin/env python
 
 """
-Contains drivers for the PDF
+Contains single driver and driver class for the probability density functions
 """
 
-from ..commonDrivers import CommonPostProcessingDriver
-from ..superClasses import PointsSuperClass
+from ..superClasses import DriverPointsSuperClass
+from .collectAndCalcPDF import CollectAndCalcPDF
 from .plotPDF import PlotPDF
-from .calcPDF import calcPDF
+from multiprocessing import Process
+
+#{{{driverPDF
+def driverPDF(collectPaths     ,\
+              varName          ,\
+              convertToPhysical,\
+              mode             ,\
+              indicesArgs      ,\
+              indicesKwargs    ,\
+              plotSuperKwargs  ,\
+             ):
+    #{{{docstring
+    """
+    Driver for plotting probability density functions.
+
+    Parameters
+    ----------
+    collectPaths : tuple
+        Paths to collect from.
+        The corresponind 't_array' of the paths must be in ascending order.
+    varName : str
+        The variable name which will be used.
+    convertToPhysical : bool
+        Whether or not to convert to physical units.
+    mode : ["normal"|"fluct"]
+        If mode is "normal" the raw data is given as an output.
+        If mode is "fluct" the fluctuations are given as an output.
+    indicesArgs : tuple
+        Contains xInd, yInd and zInd.
+        See CollectAndCalcPointsSuperClass.setIndices for details.
+    indicesKwargs : dict
+        Contains tslice, nPoints, equallySpace and steadyStatePath.
+        See CollectAndCalcPointsSuperClass.setIndices for details.
+    plotSuperKwargs : dict
+        Keyword arguments for the plot super class.
+    """
+    #}}}
+
+    # Create collect object
+    ccPDF = CollectAndCalcPDF(collectPaths                         ,\
+                              mode              = mode             ,\
+                              convertToPhysical = convertToPhysical,\
+                             )
+
+    # Set the slice
+    ccPDF.setIndices(*indicesArgs, **indicesKwargs)
+
+    # Set name
+    ccPDF.setVarName(varName)
+
+    # Execute the collection
+    tt = ccPDF.executeCollectAndCalc()
+    tt = ccPDF.convertTo1D(PDF)
+
+    # Calculate the PDF
+    PDF = ccPDF.calcPDF(tt)
+
+    # Plot
+    pPDF = PlotPDF(ccPDF.uc         ,\
+                   **plotSuperKwargs)
+    pPDF.setData(PDF, mode)
+    pPDF.plotSaveShowPDF()
+#}}}
 
 #{{{DriverPDF
-class DriverPDF(PointsSuperClass, CommonPostProcessingDriver):
+class DriverPDF(DriverPointsSuperClass):
     """
-    Class which handles the PDF data.
+    Class for driving of the plotting of the probability density functions.
     """
 
     #{{{Constructor
-    def __init__(self,\
-                 *args,\
+    def __init__(self                       ,\
+                 dmp_folders                ,\
+                 indicesArgs                ,\
+                 indicesKwargs              ,\
+                 plotSuperKwargs            ,\
+                 varName           = "n"    ,\
+                 mode              = "fluct",\
                  **kwargs):
         #{{{docstring
         """
         This constructor:
-
-        1. Calls the parent constructor
-        2. Sets pltSize
+            * Calls the parent class
+            * Set the member data
+            * Updates the plotSuperKwargs
 
         Parameters
         ----------
-        *args : positional arguments
-            See the parent constructor for details.
+        dmp_folders : tuple
+            Tuple of the dmp_folder (output from bout_runners).
+        indicesArgs : tuple
+            Contains xInd, yInd and zInd.
+            See CollectAndCalcPointsSuperClass.setIndices for details.
+        indicesKwargs : dict
+            Contains tslice, nPoints, equallySpace and steadyStatePath.
+            See CollectAndCalcPointsSuperClass.setIndices for details.
+        plotSuperKwargs : dict
+            Keyword arguments for the plot super class.
+        varName : str
+            Name of variable to collect and plot
+        mode : ["normal"|"fluct"]
+            If mode is "normal" the raw data is given as an output.
+            If mode is "fluct" the fluctuations are given as an output.
         **kwargs : keyword arguments
-            See the parent constructor for details.
+            See parent class for details.
         """
         #}}}
 
         # Call the constructor of the parent class
-        super().__init__(*args, **kwargs)
+        super().__init__(dmp_folders, **kwargs)
 
-        # Set member data
-        self._pltSize = (12, 9)
+        # Set the member data
+        self._varName       = varName
+        self._mode          = mode
+        self._indicesArgs   = indicesArgs
+        self._indicesKwargs = indicesKwargs
 
-        # Placeholder for the timeTrace
-        self._PDF = None
+        # Update the plotSuperKwargs dict
+        plotSuperKwargs.update({"dmp_folders":dmp_folders})
+        plotSuperKwargs.update({"plotType"   :"PDFs"})
+        self._plotSuperKwargs = plotSuperKwargs
     #}}}
 
-    #{{{getPDF
-    def getPDF(self):
-        """Obtain the PDF"""
-        # Create the probes
-        self._PDF = calcPDF(self._paths,\
-                            self._varName,\
-                            self._xInd,\
-                            self._yInd,\
-                            self._zInd,\
-                            converToPhysical = self.convertToPhysical,\
-                            mode             = self._mode,\
-                            tSlice           = self._tSlice,\
-                            )
-    #}}}
-
-    #{{{plotPDF
-    def plotPDF(self):
-        """Plots the PDF"""
-
-        # Calculate the probes if not already done
-        if self._PDF == None:
-            self.getPDF()
-
-        # Create the energyPlotter
-        PDFPlotter = PlotPDF(\
-                self._paths                                ,\
-                self._PDF                                  ,\
-                convertToPhysical = self.convertToPhysical,\
-                showPlot          = self._showPlot         ,\
-                savePlot          = self._savePlot         ,\
-                extension         = self._extension        ,\
-                savePath          = self._savePath         ,\
-                pltSize           = self._pltSize          ,\
-                                  )
-
-        PDFPlotter.plotPDF()
+    #{{{driverPDF
+    def driverPDF(self):
+        #{{{docstring
+        """
+        Wrapper to driverPDF
+        """
+        #}}}
+        args =  (\
+                 self._collectPaths    ,\
+                 self._varName         ,\
+                 self.convertToPhysical,\
+                 self._mode            ,\
+                 self._indicesArgs     ,\
+                 self._indicesKwargs   ,\
+                 self._plotSuperKwargs ,\
+                )
+        if self._useSubProcess:
+            processes = Process(target = driverPDF, args = args)
+            processes.start()
+        else:
+            driverPDF(*args)
     #}}}
 #}}}
--- celmaRC/common/CELMAPy/PDF/plotPDF.py
+++ celmaRC/common/CELMAPy/PDF/plotPDF.py
@@ -14,79 +14,137 @@ class PlotPDF(PlotsSuperClass):
     Class which contains the PDF data and the plotting configuration.
     """
 
-    #{{{__init___
-    def __init__(self    ,\
-                 *args   ,\
-                 PDF     ,\
-                 mode    ,\
-                 **kwargs):
+    #{{{constructor
+    def __init__(self, *args, pltSize = (15,10), **kwargs):
         #{{{docstring
         """
         This constructor:
 
-        1. Calls the parent constructor
-        2. Sets the member data
-        3. Prepares the labels
+        * Calls the parent constructor
 
         Parameters
         ----------
-        *args : positional arguments
-            See the parent constructor for details.
-        PDF : dict
-            Dictionary where the keys are on the form "rho,theta,z".
-            The value is a dict containing of
-            {"pdfX":pdfX, "pdfY":"pdfY"}
-        mode : ["normal"|"fluct"]
-            What mode the input is given in.
-        **kwargs : keyword arguments
-            See the parent constructor for details.
+        pltSize : tuple
+            The size of the plot
         """
         #}}}
 
         # Call the constructor of the parent class
         super().__init__(*args, **kwargs)
 
+        # Set the plot size
+        self._pltSize = pltSize
+    #}}}
+
+    #{{{setData
+    def setData(self, PDF, mode):
+        #{{{docstring
+        """
+        Sets the time traces to be plotted.
+
+        This function also sets the variable labels, colors and the save name.
+
+        Parameters
+        ----------
+        PDF : dict
+            Dictionary where the keys are on the form "rho,theta,z".
+            The value is a dict containing of
+            {"pdfX":pdfX, "pdfY":"pdfY"}
+        mode : ["normal"|"fluct"]
+            What mode the input is given in.
+        """
+        #}}}
+
         # Set the member data
-        self._PDF    = PDF
-        self._colors = seqCMap3(np.linspace(0, 1, len(PDF.keys())))
+        self._PDF = PDF
+        self._mode = mode
 
         # Obtain the varname
         ind  = PDF.keys()[0]
         keys = PDF[ind].keys()
         self._varName = [var[:-4] for var in keys if "PDF" in var][0]
 
+        # Obtain the color (pad away brigthest colors)
+        pad = 3
+        self._colors = seqCMap3(np.linspace(0, 1, len(PDF.keys())+pad))
+
+        self._prepareLabels()
+
         # Set the labels
+        pltVarName = self._ph.getVarPltName(self._varName)
+
+        # Make a unitsOrNormDict which can be altered
+        # If not(self.uc.convertToPhysical) we need to manually modify
+        # the normalization argument
+        unitsOrNormDict = self.uc.conversionDict[self._varName]
+        if not(self.uc.convertToPhysical):
+            if r"/" in unitsOrNormDict["normalization"]:
+                if unitsOrNormDict["normalization"][0] == r"/":
+                    unitsOrNormDict["normalization"] =\
+                        unitsOrNormDict["normalization"][1:]
+                else:
+                    # Swap the nominator and denominator
+                    tmp = unitsOrNormDict["normalization"].split(r"/")
+                    unitsOrNormDict["normalization"] =\
+                            r"/".join((tmp[1],tmp[0]))
+            else:
+                # Add division sign
+                unitsOrNormDict["normalization"] =\
+                        r"/"+unitsOrNormDict["normalization"]
+
+        self._varLabel = self._varLabelTemplate.\
+            format(pltVarName, **unitsOrNormDict)
+
+        self._xLabel = self._varLabelTemplate.\
+            format(pltVarName, **self.uc.conversionDict[self._varName])
+
+        # Set the fileName
+        self._fileName =\
+            os.path.join(self._savePath,\
+                "{}-{}-{}".format(self._varName, "PDF", self._fluctName))
+
+        if self._extension is None:
+            self._extension = "png"
+
+        self._fileName = "{}.{}".format(self._fileName, self._extension)
+    #}}}
+
+    #{{{_prepareLabels
+    def _prepareLabels(self):
+        """
+        Prepares the labels for plotting.
+        """
+        # Set var label templates
         # NOTE: The probability does not have any units, but in order to
         #       obtain the probability one has to integrate over
         #       the PDF.
         #       Thus will the PDF have the dimension of the inverse of
         #       what is on the x axis
-        pltVarName   = self.ph.getVarPltName(self._varname)
-
-        norm  = self.uc.conversionDict[self._varName]["normalization"]
-        units = self.uc.conversionDict[self._varName]["units"]
-
-        # Set the variable label
-        if self.convertToPhysical:
-            if mode == "normal":
-                self._xLabel = r"${}$ $[{}]$"
-            elif mode == "fluct":
-                self._xLabel = r"$\tilde{{{}}}$ $[{}]$"
-            self._xLabel = self._xLabel.format(pltVarName, units)
-            self._yLabel = r"$\mathrm{{PDF}}(\tilde{{{}}})$".\
-                    format(pltVarName)
+        if self.uc.convertToPhysical:
+            unitsOrNormalization       = " $[{units}]$"
+            xLabelunitsOrNormalization = " $[1/{units}]$"
         else:
-            if mode == "normal":
-                self._xLabel = r"${}{}$"
-            elif mode == "fluct":
-                self._xLabel = r"$\tilde{{{}}}{}$"
-            self._xLabel = self._xLabel.format(pltVarName, norm)
-            self._yLabel = r"$\mathrm{{PDF}}(\tilde{{{}}}{})$".\
-                    format(pltVarName, norm)
+            unitsOrNormalization       = "${normalization}$"
+            # NOTE: this will be treated at a later stage
+            xLabelunitsOrNormalization = "${normalization}$"
+        if self._mode == "normal":
+            self._varLabelTemplate =\
+                r"$\mathrm{{{PDF}}}({{}})${}".format(unitsOrNormalization)
+            self._xLabel = r"${{}}${}".format(unitsOrNormalization)
+            self._fluctName = ""
+        elif self._mode == "fluct":
+            self._varLabelTemplate =\
+                r"$\mathrm{{{{{{{PDF}}}}}}}(\widetilde{{{{{{}}}}}})${}".\
+                format(unitsOrNormalization)
+            self._xLabel = r"$\widetilde{{{{{{}}}}}}${}".\
+                               format(xLabelunitsOrNormalization)
+            self._fluctName = "fluct"
+        else:
+            raise NotImplementedError("'{}'-mode not implemented.")
     #}}}
 
-    #{{{plotPDFs
-    def plotPDFs(self):
+    #{{{plotSaveShowPDF
+    def plotSaveShowPDF(self):
         """ Plots the probability density function."""
 
         # Create the plot
