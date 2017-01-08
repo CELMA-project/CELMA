cd265ff01ec483a35fad7d8aa9cdd1f73f720c91
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-08 20:30:07 +0100

    Checkpoint: Separating implementation interface
    Separated for the plotSubmitter
    
    
    Former-commit-id: 1b6d82ea15df44e3f5326c43c4298b6df33cbc4f

--- celmaRC/1-CELMA-RC/PBSPlot-CSDXMagFieldScanAr.py
+++ celmaRC/1-CELMA-RC/PBSPlot-CSDXMagFieldScanAr.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-"""Driver which runs using PBS."""
-
-import pickle
-from time import sleep
+"""Driver which plots the results of the simulations."""
 
 import os, sys
 # If we add to sys.path, then it must be an absolute path
@@ -11,349 +8,42 @@ commonDir = os.path.abspath("./../common")
 # Sys path is a list of system paths
 sys.path.append(commonDir)
 
-from CELMAPy.driverHelpers import PBSSubmitter, pathMerger
-from standardPlots import (\
-                           combinedPlotsPlot,\
-                           fourierModesPlot ,\
-                           growthRatesPlot  ,\
-                           energyPlot       ,\
-                           performancePlot  ,\
-                           posOfFluctPlot   ,\
-                           PSD2DPlot        ,\
-                           skewKurtPlot     ,\
-                           zonalFlowPlot    ,\
-                           )
+from standardPlots import PlotSubmitter
 
-#{{{findSlices
-def findSlices(dmp_folders):
-    """
-    Finds the slices
-
-    Parameters
-    ----------
-    dmp_folders : str
-        Dump folder under investigation
-    """
-    found = False
-    for tkey in tSlices.keys():
-        if tkey in dmp_folders:
-            tSlice = tSlices[tkey]
-            found = True
-            break
-    if not(found):
-        raise ValueError("Could not find correct slice")
-
-    return tSlice
-#}}}
-
-#{{{runFourierModes
-def runFourierModes(sliced = False):
-    """
-    Runs the fourier modes
-
-    Parameters
-    ----------
-    sliced : bool
-        Whether or not to slice the time
-    """
-
-    loopOver = zip(dmpFolders["turbulence"],\
-                   dmpFolders["expand"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, steadyStatePath, key, nr in loopOver:
-
-        if sliced:
-            tSlice = findSlices(dmp_folders)
-            if tSlice is None:
-                continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders   = (dmp_folders,)
-        args = (dmp_folders, collectPaths, steadyStatePath)
-        if sliced:
-            kwargs = {"tSlice":(tSlice,)}
-            sub.setJobName("fourierModesSliced{}".format(nr))
-        else:
-            kwargs = {}
-            sub.setJobName("fourierModes{}".format(nr))
-        sub.submitFunction(fourierModesPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runGrowthRates
-def runGrowthRates():
-    """
-    Runs the growth rates
-    """
-
-    # NOTE: The ordering of param is in descending order (because of the
-    #       organization in PBSScan)
-    dmp_folders = (mergeFromLinear["param0"][-1],)
-    keys = tuple(sorted(list(mergeFromLinear.keys())))
-    scanCollectPaths = tuple(mergeFromLinear[key] for key in keys)
-    steadyStatePaths = dmpFolders["expand"]
-
-    # NOTE: The ordering is of the keys are in descending order (because
-    #       of the organization in PBSScan)
-    growthTSlicesKeys = tuple(sorted(list(tSlices.keys()), reverse=True))
-    growthTSlices = tuple(tSlices[key] for key in growthTSlicesKeys)
-
-    args = (dmp_folders     ,\
-            scanCollectPaths,\
-            steadyStatePaths,\
-            scanParameter   ,\
-            growthTSlices)
-    sub.setJobName("growthRates")
-    sub.submitFunction(growthRatesPlot, args=args)
-#}}}
-
-#{{{runEnergy
-def runEnergy(sliced=False):
-    """
-    Runs the energies
-
-    Parameters
-    ----------
-    sliced : bool
-        Whether or not to slice the time
-    """
-    loopOver = zip(dmpFolders["turbulence"], paramKeys, rJobs)
-    for dmp_folders, key, nr in loopOver:
-
-        if sliced:
-            # Find tSlice
-            tSlice = findSlices(dmp_folders)
-            if tSlice is None:
-                continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths)
-        if sliced:
-            kwargs = {"tSlice":tSlice}
-            sub.setJobName("energySliced{}".format(nr))
-        else:
-            kwargs = {}
-            sub.setJobName("energy{}".format(nr))
-        sub.submitFunction(energyPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runPosOfFluct
-def runPosOfFluct():
-    """
-    Runs the position of fluct
-    """
-
-    loopOver = zip(dmpFolders["turbulence"],\
-                   dmpFolders["expand"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, steadyStatePath, key, nr in loopOver:
-
-        # Find tSlice
-        tSlice = findSlices(dmp_folders)
-        if tSlice is None:
-            continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths, steadyStatePath)
-        kwargs = {"tSlice":tSlice}
-        sub.setJobName("posOfFluctSliced{}".format(nr))
-        sub.submitFunction(posOfFluctPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runPSD2D
-def runPSD2D():
-    """
-    Runs the PSD2D
-    """
-
-    loopOver = zip(dmpFolders["turbulence"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, key, nr in loopOver:
-
-        # Find tSlice
-        tSlice = findSlices(dmp_folders)
-        if tSlice is None:
-            continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths)
-        kwargs = {"tSlice":tSlice}
-        sub.setJobName("PSD2DPlotSliced{}".format(nr))
-        sub.submitFunction(PSD2DPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runSkewKurt
-def runSkewKurt():
-    """
-    Runs the skewness and kurtosis
-    """
-
-    loopOver = zip(dmpFolders["turbulence"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, key, nr in loopOver:
-
-        # Find tSlice
-        tSlice = findSlices(dmp_folders)
-        if tSlice is None:
-            continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths)
-        kwargs = {"tSlice":tSlice}
-        sub.setJobName("skewnessKurtosisSliced{}".format(nr))
-        sub.submitFunction(skewKurtPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runZonalFlow
-def runZonalFlow():
-    """
-    Runs the skewness and kurtosis
-    """
-    loopOver = zip(dmpFolders["turbulence"],\
-                   dmpFolders["expand"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, steadyStatePath, key, nr in loopOver:
-
-        # Find tSlice
-        tSlice = findSlices(dmp_folders)
-        if tSlice is None:
-            continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths, steadyStatePath)
-        kwargs = {"tSlice":tSlice}
-        sub.setJobName("zonalFlowSliced{}".format(nr))
-        sub.submitFunction(zonalFlowPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runCominedPlots
-def runCominedPlots():
-    """
-    Runs the combined plots
-    """
-    loopOver = zip(dmpFolders["turbulence"],\
-                   dmpFolders["expand"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, steadyStatePath, key, nr in loopOver:
-
-        # Find tSlice
-        tSlice = findSlices(dmp_folders)
-        if tSlice is None:
-            continue
-
-        collectPaths = mergeFromLinear[key]
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths, steadyStatePath)
-        kwargs = {"tSlice":tSlice}
-        sub.setJobName("combinedPlotsSliced{}".format(nr))
-        sub.submitFunction(combinedPlotsPlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{runPerformance
-def runPerformance(allFolders=False):
-    """
-    Runs the performance plots
-
-    Parameters
-    ----------
-    allFolders : bool
-        If "init" and "expand" should be included in the plot.
-    """
-    loopOver = zip(dmpFolders["turbulence"],\
-                   paramKeys,\
-                   rJobs)
-    for dmp_folders, key, nr in loopOver:
-
-        if allFolders:
-            collectPaths = mergeAll[key]
-            sub.setJobName("performanceAll{}".format(nr))
-            kwargs = {"allFolders":True}
-        else:
-            collectPaths = mergeFromLinear[key]
-            sub.setJobName("performance{}".format(nr))
-            kwargs = {"allFolders":False}
-        dmp_folders  = (dmp_folders,)
-        args = (dmp_folders, collectPaths)
-        sub.submitFunction(performancePlot, args=args, kwargs=kwargs)
-        # Sleep 1.5 seconds to ensure that tmp files will have different names
-        sleep(1.5)
-#}}}
-
-#{{{Globals
 directory = "CSDXMagFieldScanAr"
 scanParameter = "B0"
 
-with open(os.path.join(directory, "dmpFoldersDict.pickle"), "rb") as f:
-        dmpFolders = pickle.load(f)
+# Create the plotSubmitter
+pltSub = PlotSubmitter(directory, scanParameter)
 
-mergeFromLinear =\
-        pathMerger(dmpFolders, ("linear", "turbulence", "extraTurbulence"))
+# Run the fourier modes
+# pltSub.runFourierModes(sliced=False)
+# Set linear slices and plot the sliced fourier modes
+tSlices = {\
+           "B0_0.02":slice(80,240)  ,\
+           "B0_0.04":slice(800,1250),\
+           "B0_0.06":slice(180,300) ,\
+           "B0_0.08":slice(100,225) ,\
+           "B0_0.1" :slice(80,210)  ,\
+           }
+plt.setLinearPhaseTSlices(tSlices)
 
-mergeAll =\
-        pathMerger(dmpFolders,\
-                  ("init", "expand", "linear", "turbulence", "extraTurbulence"))
-
-paramKeys = tuple(sorted(list(mergeFromLinear.keys())))
-rJobs     = range(len(paramKeys))
-
-# Generate the submitter
-sub = PBSSubmitter()
-sub.setNodes(nodes=1, ppn=2)
-sub.setQueue("xpresq")
-sub.setWalltime("00:15:00")
-#}}}
-
-if __name__ == "__main__":
-
-    # Run the fourier modes
-    # runFourierModes(sliced=False)
-    # Set linear slices and plot the sliced fourier modes
-    tSlices = {\
-               "B0_0.02":slice(80,240)  ,\
-               "B0_0.04":slice(800,1250),\
-               "B0_0.06":slice(180,300) ,\
-               "B0_0.08":slice(100,225) ,\
-               "B0_0.1" :slice(80,210)  ,\
-               }
-    # runFourierModes(sliced=True)
-    # runGrowthRates()
-    # runEnergy(sliced=False)
-    tSlices = {\
-               "B0_0.02":None,\
-               "B0_0.04":None,\
-               "B0_0.06":slice(1200,None),\
-               "B0_0.08":slice(1000,None),\
-               "B0_0.1" :None,\
-               }
-    # runEnergy(sliced=True)
-    # runPosOfFluct()
-    # runPSD2D()
-    # runSkewKurt()
-    # runZonalFlow()
-    # runCominedPlots()
-    runPerformance(allFolders=False)
-    runPerformance(allFolders=True)
+# pltSub.runFourierModes(sliced=True)
+# pltSub.runGrowthRates()
+# pltSub.runEnergy(sliced=False)
+tSlices = {\
+           "B0_0.02":None,\
+           "B0_0.04":None,\
+           "B0_0.06":slice(1200,None),\
+           "B0_0.08":slice(1000,None),\
+           "B0_0.1" :None,\
+           }
+plt.setSaturatedTurbulenceTSlices(tSlices)
+# pltSub.runEnergy(sliced=True)
+# pltSub.runPosOfFluct()
+# pltSub.runPSD2D()
+# pltSub.runSkewKurt()
+# pltSub.runZonalFlow()
+# pltSub.runCominedPlots()
+pltSub.runPerformance(allFolders=False)
+pltSub.runPerformance(allFolders=True)
---
+++ celmaRC/common/standardPlots/plotSubmitter.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+
+"""Contains the PlotSubmitter class."""
+
+import pickle
+from time import sleep
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath("./../")
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from CELMAPy.driverHelpers import PBSSubmitter, pathMerger
+from .combinedPlots import combinedPlotsPlot
+from .fourierModes import fourierModesPlot
+from .growthRates import growthRatesPlot
+from .energy import energyPlot
+from .performance import performancePlot
+from .posOfFluct import posOfFluctPlot
+from .PSD2D import PSD2DPlot
+from .skewKurt import skewKurtPlot
+from .zonalFlow import zonalFlowPlot
+
+#{{{PlotSubmitter
+class PlotSubmitter(object)
+    """Class used to submit the standard plots"""
+
+    #{{{constructor
+    def __init__(self, directory, scanParameter):
+        #{{{docstring
+        """
+        Constructor for the PlotSubmitter class.
+
+        The constructor will:
+            * Load the pickle containing the dmp_folders.
+            * Generate the submitter to be used
+
+        Parameters
+        ----------
+        directory : str
+            Root directory of the simulations.
+        scanParameter : str
+            The scan parameter.
+        """
+        #}}}
+
+        self._scanParameter = scanParameter
+
+        with open(os.path.join(directory, "dmpFoldersDict.pickle"), "rb") as f:
+                dmpFolders = pickle.load(f)
+
+        mergeFromLinear =\
+                pathMerger(dmpFolders, ("linear", "turbulence", "extraTurbulence"))
+
+        mergeAll =\
+                pathMerger(dmpFolders,\
+                          ("init", "expand", "linear", "turbulence", "extraTurbulence"))
+
+        paramKeys = tuple(sorted(list(mergeFromLinear.keys())))
+        rJobs     = range(len(paramKeys))
+
+        # Generate the submitter
+        sub = PBSSubmitter()
+        sub.setNodes(nodes=1, ppn=2)
+        sub.setQueue("xpresq")
+        sub.setWalltime("00:15:00")
+    #}}}
+
+    #{{{_findSlices
+    def _findSlices(self, dmp_folders):
+        #{{{docstring
+        """
+        Finds the temporal slices to use for the dmp_folder.
+
+        Parameters
+        ----------
+        dmp_folders : str
+            Dump folder under investigation
+
+        Returns
+        -------
+        tSlice : slice
+            The tSlice corresponding to the dmp_folder.
+        """
+        #}}}
+
+        found = False
+        for tkey in tSlices.keys():
+            if tkey in dmp_folders:
+                tSlice = tSlices[tkey]
+                found = True
+                break
+        if not(found):
+            raise ValueError("Could not find correct slice")
+
+        return tSlice
+    #}}}
+
+    #{{{runFourierModes
+    def runFourierModes(self, sliced = False):
+        #{{{docstring
+        """
+        Runs the fourier modes
+
+        Parameters
+        ----------
+        sliced : bool
+            Whether or not to slice the time
+        """
+        #}}}
+
+        loopOver = zip(dmpFolders["turbulence"],\
+                       dmpFolders["expand"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, steadyStatePath, key, nr in loopOver:
+
+            if sliced:
+                tSlice = _findSlices(dmp_folders)
+                if tSlice is None:
+                    continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders   = (dmp_folders,)
+            args = (dmp_folders, collectPaths, steadyStatePath)
+            if sliced:
+                kwargs = {"tSlice":(tSlice,)}
+                sub.setJobName("fourierModesSliced{}".format(nr))
+            else:
+                kwargs = {}
+                sub.setJobName("fourierModes{}".format(nr))
+            sub.submitFunction(fourierModesPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runGrowthRates
+    def runGrowthRates(self):
+        """
+        Runs the growth rates
+        """
+
+        # NOTE: The ordering of param is in descending order (because of the
+        #       organization in PBSScan)
+        dmp_folders = (mergeFromLinear["param0"][-1],)
+        keys = tuple(sorted(list(mergeFromLinear.keys())))
+        scanCollectPaths = tuple(mergeFromLinear[key] for key in keys)
+        steadyStatePaths = dmpFolders["expand"]
+
+        # NOTE: The ordering is of the keys are in descending order (because
+        #       of the organization in PBSScan)
+        growthTSlicesKeys = tuple(sorted(list(tSlices.keys()), reverse=True))
+        growthTSlices = tuple(tSlices[key] for key in growthTSlicesKeys)
+
+        args = (dmp_folders     ,\
+                scanCollectPaths,\
+                steadyStatePaths,\
+                scanParameter   ,\
+                growthTSlices)
+        sub.setJobName("growthRates")
+        sub.submitFunction(growthRatesPlot, args=args)
+    #}}}
+
+    #{{{runEnergy
+    def runEnergy(self, sliced=False):
+        """
+        Runs the energies
+
+        Parameters
+        ----------
+        sliced : bool
+            Whether or not to slice the time
+        """
+        loopOver = zip(dmpFolders["turbulence"], paramKeys, rJobs)
+        for dmp_folders, key, nr in loopOver:
+
+            if sliced:
+                # Find tSlice
+                tSlice = _findSlices(dmp_folders)
+                if tSlice is None:
+                    continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths)
+            if sliced:
+                kwargs = {"tSlice":tSlice}
+                sub.setJobName("energySliced{}".format(nr))
+            else:
+                kwargs = {}
+                sub.setJobName("energy{}".format(nr))
+            sub.submitFunction(energyPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runPosOfFluct
+    def runPosOfFluct(self):
+        """
+        Runs the position of fluct
+        """
+
+        loopOver = zip(dmpFolders["turbulence"],\
+                       dmpFolders["expand"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, steadyStatePath, key, nr in loopOver:
+
+            # Find tSlice
+            tSlice = _findSlices(dmp_folders)
+            if tSlice is None:
+                continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths, steadyStatePath)
+            kwargs = {"tSlice":tSlice}
+            sub.setJobName("posOfFluctSliced{}".format(nr))
+            sub.submitFunction(posOfFluctPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runPSD2D
+    def runPSD2D(self):
+        """
+        Runs the PSD2D
+        """
+
+        loopOver = zip(dmpFolders["turbulence"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, key, nr in loopOver:
+
+            # Find tSlice
+            tSlice = _findSlices(dmp_folders)
+            if tSlice is None:
+                continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths)
+            kwargs = {"tSlice":tSlice}
+            sub.setJobName("PSD2DPlotSliced{}".format(nr))
+            sub.submitFunction(PSD2DPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runSkewKurt
+    def runSkewKurt(self):
+        """
+        Runs the skewness and kurtosis
+        """
+
+        loopOver = zip(dmpFolders["turbulence"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, key, nr in loopOver:
+
+            # Find tSlice
+            tSlice = _findSlices(dmp_folders)
+            if tSlice is None:
+                continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths)
+            kwargs = {"tSlice":tSlice}
+            sub.setJobName("skewnessKurtosisSliced{}".format(nr))
+            sub.submitFunction(skewKurtPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runZonalFlow
+    def runZonalFlow(self):
+        """
+        Runs the skewness and kurtosis
+        """
+        loopOver = zip(dmpFolders["turbulence"],\
+                       dmpFolders["expand"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, steadyStatePath, key, nr in loopOver:
+
+            # Find tSlice
+            tSlice = _findSlices(dmp_folders)
+            if tSlice is None:
+                continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths, steadyStatePath)
+            kwargs = {"tSlice":tSlice}
+            sub.setJobName("zonalFlowSliced{}".format(nr))
+            sub.submitFunction(zonalFlowPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runCominedPlots
+    def runCominedPlots(self):
+        """
+        Runs the combined plots
+        """
+        loopOver = zip(dmpFolders["turbulence"],\
+                       dmpFolders["expand"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, steadyStatePath, key, nr in loopOver:
+
+            # Find tSlice
+            tSlice = _findSlices(dmp_folders)
+            if tSlice is None:
+                continue
+
+            collectPaths = mergeFromLinear[key]
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths, steadyStatePath)
+            kwargs = {"tSlice":tSlice}
+            sub.setJobName("combinedPlotsSliced{}".format(nr))
+            sub.submitFunction(combinedPlotsPlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+
+    #{{{runPerformance
+    def runPerformance(self, allFolders=False):
+        #{{{docstring
+        """
+        Runs the performance plots
+
+        Parameters
+        ----------
+        allFolders : bool
+            If "init" and "expand" should be included in the plot.
+        """
+        #}}}
+        loopOver = zip(dmpFolders["turbulence"],\
+                       paramKeys,\
+                       rJobs)
+        for dmp_folders, key, nr in loopOver:
+
+            if allFolders:
+                collectPaths = mergeAll[key]
+                sub.setJobName("performanceAll{}".format(nr))
+                kwargs = {"allFolders":True}
+            else:
+                collectPaths = mergeFromLinear[key]
+                sub.setJobName("performance{}".format(nr))
+                kwargs = {"allFolders":False}
+            dmp_folders  = (dmp_folders,)
+            args = (dmp_folders, collectPaths)
+            sub.submitFunction(performancePlot, args=args, kwargs=kwargs)
+            # Sleep 1.5 seconds to ensure that tmp files will have different names
+            sleep(1.5)
+    #}}}
+#}}}
