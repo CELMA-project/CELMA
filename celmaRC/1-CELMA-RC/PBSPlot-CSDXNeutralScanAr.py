b9093237773867e2d22a0a8205b0865e69f55cc2
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-09 14:44:17 +0100

    New plot and scan files
    Former-commit-id: 23f49dd5dd0d154c27908d56a85fa47ec4947914

---
+++ celmaRC/1-CELMA-RC/PBSPlot-CSDXNeutralScanAr.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""Driver which plots the results of the simulations."""
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath("./../common")
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from standardPlots import PlotSubmitter
+
+directory = "CSDXNeutralScanAr"
+scanParameter = "nn"
+
+# Create the plotSubmitter
+pltSub = PlotSubmitter(directory, scanParameter)
+pltSub.sub.setMisc(logPath = os.path.join(directory,"postLogs"))
+
+# Set linear slices
+tSlices = {\
+           "nn_5e+19"                 : slice(180, 270),\
+           "nn_2.5e+19"               : slice(150, 240),\
+           "nn_1.6666666666666668e+19": slice(150, 300),\
+           "nn_1.25e+19"              : slice(180, 270),\
+           }
+pltSub.setLinearPhaseTSlices(tSlices)
+
+# Set saturated turbulence slices
+tSlices = {\
+           "nn_5e+19"                 : slice(1000, None),\
+           "nn_2.5e+19"               : slice(1000, None),\
+           "nn_1.6666666666666668e+19": slice(1200, None),\
+           "nn_1.25e+19"              : slice(1000, None),\
+           }
+pltSub.setSatTurbTSlices(tSlices)
+
+# Run the post-processing
+pltSub.updatePlotSuperKwargs({"extension" : "pdf"})
+pltSub.runCominedPlots()
+pltSub.runEnergy(sliced=False)
+pltSub.runEnergy(sliced=True)
+pltSub.runFourierModes(sliced=False)
+pltSub.runFourierModes(sliced=True)
+pltSub.runGrowthRates()
+pltSub.runPerformance(allFolders=False)
+pltSub.runPerformance(allFolders=True)
+pltSub.runPosOfFluct()
+pltSub.runPSD2D()
+pltSub.runSkewKurt()
+pltSub.runZonalFlow()
+
+# Run the animations
+pltSub.updatePlotSuperKwargs({"extension" : None})
+pltSub.sub.setWalltime("01:00:00")
+pltSub.runFields1DAnim()
+pltSub.sub.setQueue("fatq")
+pltSub.sub.setWalltime("06:00:00")
+pltSub.runFields2DAnim(fluct=True)
+pltSub.runFields2DAnim(fluct=False)
