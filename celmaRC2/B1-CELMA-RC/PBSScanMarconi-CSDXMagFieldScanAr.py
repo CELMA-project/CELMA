a7e381f722a9f659144c8659392b0433bc789e01
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-12 18:02:02 +0100

    Rename for better distinguish
    Former-commit-id: 901be274b6af9f2193589a99c9fcc1f5cbaf8887

--- celmaRC2/B1-CELMA-RC/PBSScanMarconi-CSDXMagFieldScanAr.py
+++ celmaRC2/B1-CELMA-RC/PBSScanMarconi-CSDXMagFieldScanAr.py
@@ -10,7 +10,7 @@ sys.path.append(commonDir)
 
 from CELMAPy.scanDriver import ScanDriver
 
-directory = "CSDXMagFieldScanAr"
+directory = "BousCSDXMagFieldScanAr"
 
 # Create object
 scanB0 = ScanDriver(directory)
