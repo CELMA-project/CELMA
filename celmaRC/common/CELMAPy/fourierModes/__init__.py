4ac9d6c15958cc408e6d6ef9c83826c92c77aabb
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-16 10:34:01 +0100

    Symbolic link of fourier modes

--- celmaRC/common/CELMAPy/fourierModes/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Init-file for fourier modes
-"""
-
-from .collectAndCalcFourierModes import CollectAndCalcFourierModes
-from .driverFourierModes import DriverFourierModes, driverFourierModes
-from .plotFourierModes import PlotFourierModes
