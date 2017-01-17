3b96326ae97889f171da3e551543fa2b8b95047a
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-17 20:07:33 +0100

    Checkpoint for analytic gr

--- celmaRC2/common/CELMAPy/growthRates/__init__.py
+++ celmaRC2/common/CELMAPy/growthRates/__init__.py
@@ -4,6 +4,24 @@
 Init-file for growth rates
 """
 
+from .analyticalGrowthRates import (ellisAnalytical  ,\
+                                    pecseliAnalytical,\
+                                    calcNuPar        ,\
+                                    calcOm1          ,\
+                                    calcSigmaPar     ,\
+                                    calcEllisB       ,\
+                                    calcPecseliB     ,\
+                                    calcOmStar       ,\
+                                    calcUDE          ,\
+                                    calcRhoS         ,\
+                                    calcCS           ,\
+                                    calcOmCI         ,\
+                                    calcOmCE         ,\
+                                    )
+from .collectAndCalcAnalyticGrowthRates import CollectAndCalcAnalyticGrowthRates
 from .collectAndCalcGrowthRates import CollectAndCalcGrowthRates
+from .driverAnalyticGrowthRates import (DriverAnalyticGrowthRates,\
+                                        driverAnalyticGrowthRates,\
+                                       )
 from .driverGrowthRates import DriverGrowthRates, driverGrowthRates
 from .plotGrowthRates import PlotGrowthRates
