1ab414d24c8c6b20384a54098938346e3f43a009
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-18 09:41:55 +0100

    Commit to test the analytic growth rates
    Former-commit-id: 6b52f09582c2998fb1a6f2e86fb21e011e777d66

--- celmaRC2/common/CELMAPy/growthRates/collectAndCalcAnalyticGrowthRates.py
+++ celmaRC2/common/CELMAPy/growthRates/collectAndCalcAnalyticGrowthRates.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from ..collectAndCalcHelpers import (collectSteadyN        ,\
+from ..collectAndCalcHelpers import (DimensionsHelper      ,\
+                                     collectSteadyN        ,\
                                      DDX                   ,\
                                      getUniformSpacing     ,\
                                      getScanValue          ,\
@@ -13,7 +14,9 @@ from .analyticalGrowthRates import (calcOmCE         ,\
                                     calcUDE          ,\
                                     pecseliAnalytical,\
                                    )
+from .unitsConverter import UnitsConverter
 from boututils.datafile import DataFile
+import pandas as pd
 import scipy.constants as cst
 import os
 
@@ -65,56 +68,51 @@ class CollectAndCalcAnalyticGrowthRates(object):
         #}}}
         # Create the units convertor object
         self.uc  = UnitsConverter  (path, True)
-        self._dh = DimensionsHelper(path, uc)
+        self._dh = DimensionsHelper(path, self.uc)
     #}}}
 
-    #{{{_calcPecseliSemiAnalytical
-    def _calcPecseliSemiAnalytical(self, path, m, kz, yInd):
+    #{{{_collectForPecseliSemiAnalytical
+    def _collectForPecseliSemiAnalytical(self, path, yInd):
         #{{{docstring
         """
-        Calculates the analytical growth rates based on the dispersion
-        relation assuming Ti = 0 in
+        Collects variables needed for analytical calcualtion of the
+        Pecseli dispersion relation.
 
-        Pecseli, Hans
-        Low Frequency Waves and Turbulence in Magnetized Laboratory Plasmas and in the Ionosphere
-        IOP Publishing 2016
-
-        Gets the value of the max gradient position and the max gradient
-        from the simulations.
-
-        NOTE:
-            * Assumes singly ionized particles
-            * Assumes relationship between number of nodes and
-              wavelength.
+        See also
+        --------
+        _calcPecseliSemiAnalytical
 
         Parameters
         ----------
         path : str
             Path to collect from
-        m : int
-            Mode number
-        kz : float
-            The wavenumber of the fluctuations in z.
-            Equals 2*pi/lambda_z measured in m^-1.
-            Although not a proper wavelength, this can be inferred from
-            the 2D poloidal field plots of the fluctuations.
         yInd : int
             The parallel index to slice the data
 
         Returns
         -------
-        om : complex
-            The omega in exp(-i[k*x-omega*t])
+        omCE : float
+            The electron cyclotron resonance.
+        omCI : float
+            The ion cyclotron resonance.
+        rhoS : float
+            The hybrid radius.
+        rhoMax : float
+            The position of the maximum gradient.
+        n : float
+            The density at the maximum gradient.
+        dndx : float
+            The density gradient at the position of the maximum
+            gradient.
+        uDE : float
+            The electron diamagnetic drift.
+        nuEI : float
+            The electron-ion collision frequency.
         """
         #}}}
-
         # Get the position of the largest gradient
         indMaxGrad = findLargestRadialGradN(path)
         rhoMax = self._dh.rho[indMaxGrad]
-        if self._maxRhoInd is None:
-            self_maxRhoInd = rhoMax
-
-        ky = m/rhoMax
 
         # Calculate B
         mi   = self.uc.getNormalizationParameter("mi")
@@ -131,23 +129,16 @@ class CollectAndCalcAnalyticGrowthRates(object):
         dndx = dndx[0,indMaxGrad,yInd,0]
         Te   = self.uc.getParamFromNormDict("Te0")
         uDE  = calcUDE(Te, B, n, dndx)
-        omStar = calcOmStar(ky, uDE)
 
-        # Calculation of b
-        rhoS = self.uc.getNormalizationParameter("rhoS")
-        b    = calcPecseliB(ky, rhoS)
-
-        # Calculation of sigmaPar
+        # Needed for calculation of sigmaPar
         omCE = calcOmCE(B)
         with DataFile(os.path.join(path,"BOUT.dmp.0.nc")) as f:
             nuEI = f.read("nuEI")
-        sigmaPar = calcSigmaPar(ky, kz, omCE, omCI, nuEI)
 
-        # Test as a function of B
-        # For different mode numbers
-        om = pecseliAnalytical(omStar, b, sigmaPar)
+        # Needed for calculation of b
+        rhoS = self.uc.getNormalizationParameter("rhoS")
 
-        return om
+        return omCE, omCI, rhoS, rhoMax, n, dndx, uDE, nuEI
     #}}}
 
     #{{{getData
@@ -172,6 +163,17 @@ class CollectAndCalcAnalyticGrowthRates(object):
                 * "analyticalGR"
                 * "angularVelocity"
             over the observation "modeNr" over the observation "Scan"
+        paramDataFrame : DataFrame
+            DataFrame consisting of the variables (measured properties):
+                * "omCE"
+                * "omCI"
+                * "rhoS"
+                * "rhoMax"
+                * "n"
+                * "dndx"
+                * "uDE"
+                * "nuEI"
+            over the observation "Scan".
         positionTuple : tuple
             The tuple containing (rho, z).
             Needed in the plotting routine.
@@ -185,43 +187,65 @@ class CollectAndCalcAnalyticGrowthRates(object):
         # https://github.com/pandas-dev/pandas/blob/master/doc/cheatsheet/Pandas_Cheat_Sheet.pdf
         # http://stackoverflow.com/questions/10715965/add-one-row-in-a-pandas-dataframe
 
-        multiTuples = []
+        singleIndexTuple = []
+        multiIndexTuple  = []
         fullDict = {"analyticalGR":[], "angularVelocity":[]}
 
-        loopOver = zip(self._steadyStatePaths)
+        keys = ("omCE", "omCI", "rhoS", "rhoMax", "n", "dndx", "uDE", "nuEI")
+        paramDict = {key:[] for key in keys}
 
         # Loop over the folders
         for steadyStatePath in self._steadyStatePaths:
             # Set the units converter and the dimension helper
             self._setUcAndDh(steadyStatePath)
             # Obtain the scan value
-            scanValue = getScanValue(scanPaths, self._scanParameter)
+            scanValue = getScanValue(steadyStatePath, self._scanParameter)
 
             # Assume kz is double of the cylinder heigth (observed in
             # the fluctuations)
-            kz = 2*(self._dh.z[-1] + 0.5*(self._dh.z[-1] - self._dh.z[-2])
+            kz = 2*(self._dh.z[-1] + 0.5*(self._dh.z[-1] - self._dh.z[-2]))
 
-            for modeNr in range(len(1, nModes+1)):
+            # Collect variables
+            omCE, omCI, rhoS, rhoMax, n, dndx, uDE, nuEI =\
+                self._collectForPecseliSemiAnalytical(steadyStatePath,\
+                                                      self._yInd)
+
+            # Update the single index tuple, insert into parameter dictionary
+            singleIndexTuple.append(scanValue)
+            paramDict["omCE"]  .append(omCE)
+            paramDict["omCI"]  .append(omCI)
+            paramDict["rhoS"]  .append(rhoS)
+            paramDict["rhoMax"].append(rhoMax)
+            paramDict["n"]     .append(n)
+            paramDict["dndx"]  .append(dndx)
+            paramDict["uDE"]   .append(uDE)
+            paramDict["nuEI"]  .append(nuEI)
+
+            for m in range(len(1, nModes+1)):
                 # Fill the multiIndexTuple and the dict
-                multiTuples.append((scanValue, modeNr))
+                multiIndexTuple.append((scanValue, m))
 
-                om = self._calcPecseliSemiAnalytical(steadyStatePath,\
-                                                     modeNr         ,\
-                                                     kz             ,\
-                                                     self._yInd      )
+                # Calculate the dispersion relation
+                ky = m/rhoMax
+                omStar = calcOmStar(ky, uDE)
+                b = calcPecseliB(ky, rhoS)
+                sigmaPar = calcSigmaPar(ky, kz, omCE, omCI, nuEI)
+                om = pecseliAnalytical(omStar, b, sigmaPar)
 
                 fullDict["analyticalGR"].append(om.imag)
                 fullDict["angularVelocity"].append(om.real)
 
-        # Make the data frame
+        # Make the data frames
+        paramDataFrame = pd.DataFrame(paramDict, index=singleIndexTuple)
+
         analyticalGRDataFrame =\
             pd.DataFrame(fullDict,\
                          index=pd.MultiIndex.from_tuples(\
-                            multiTuples,
+                            multiIndexTuple,
                             names=(self._scanParameter,"modeNr")))
 
-        positionTuple = (rho[self._maxRhoInd], z[self._yInd])
+        positionTuple = (self._dh.rho[self._maxRhoInd], self._dh.z[self._yInd])
 
-        return analyticalGRDataFrame, positionTuple, self.uc
+        return analyticalGRDataFrame, paramDataFrame, positionTuple, self.uc
     #}}}
 #}}}
