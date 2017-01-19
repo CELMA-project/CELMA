61d188287a2650f08fce30a58580c9749d3ca532
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-19 15:43:45 +0100

    Bugfixes to total flux
    Former-commit-id: 482c600d98995ee20f9c5a46ae204b9aca784812

---
+++ celmaRC/1-CELMA-RC/testTotalFlux.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+"""Post-processor test for totalFlux"""
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath("./../common")
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from CELMAPy.totalFlux import DriverTotalFlux, driverTotalFlux
+
+#{{{totalFluxTest
+def totalFluxTest():
+    """
+    Runs the test for the total flux
+    """
+
+    collectPaths =\
+       (\
+        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
+        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/"\
+       )
+
+    xInd              = 16
+    yInd              = 16
+    tSlice            = None
+    mode              = "normal"
+    convertToPhysical = True
+
+    savePath          = "."
+
+    plotSuperKwargs = {\
+                        "showPlot"     : False,\
+                        "savePlot"     : True,\
+                        "savePath"     : savePath,\
+                        "savePathFunc" : None,\
+                        "extension"    : None,\
+                        "dmp_folders"  : None,\
+                       }
+
+    print("\n\nTesting total flux")
+    driverTotalFlux(collectPaths     ,\
+                    xInd             ,\
+                    yInd             ,\
+                    tSlice           ,\
+                    mode             ,\
+                    convertToPhysical,\
+                    plotSuperKwargs  ,\
+                   )
+    print("Success!\n\n")
+#}}}
+
+#{{{driverTest
+def driverTest():
+    """
+    Runs the test for the total flux
+    """
+
+    dmp_folders  = ("CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1",)
+    collectPaths =\
+       (\
+        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
+        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/"\
+       )
+
+    useSubProcess = False
+
+    varName           = "n"
+    convertToPhysical = True
+    mode              = "fluct"
+
+    xInd              = None
+    yInd              = 16
+    zInd              = 128
+    tSlice            = None
+    nPoints           = 3
+    equallySpace      = "x"
+
+    steadyStatePath   = "CSDXMagFieldScanAr/nout_2_timestep_50/nz_256/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_ownFilters_type_none_switch_useHyperViscAzVortD_False_tag_CSDXMagFieldScanAr-1-expand_0/"
+
+    indicesArgs   = (xInd, yInd, zInd)
+    indicesKwargs = {"tSlice"          : tSlice         ,\
+                     "nPoints"         : nPoints        ,\
+                     "equallySpace"    : equallySpace   ,\
+                     "steadyStatePath" : steadyStatePath,\
+                     }
+
+    plotSuperKwargs = {\
+                        "showPlot"     : False,\
+                        "savePlot"     : True,\
+                        "savePath"     : None,\
+                        "savePathFunc" : None,\
+                        "extension"    : None,\
+                        "dmp_folders"  : None,\
+                       }
+
+    print("\n\nTesting total flux driver")
+    dTT = DriverTotalFlux(
+                     # DriverTotalFlux
+                     dmp_folders                ,\
+                     indicesArgs                ,\
+                     indicesKwargs              ,\
+                     plotSuperKwargs            ,\
+                     varName           = varName,\
+                     mode              = mode   ,\
+                     # DriverPointsSuperClass
+                     convertToPhysical = convertToPhysical,\
+                     # DriverSuperClass
+                     collectPaths  = collectPaths ,\
+                     useSubProcess = useSubProcess,\
+                          )
+    dTT.driverTotalFlux()
+    print("Success!\n\n")
+#}}}
+
+if __name__ == "__main__":
+    driverTest()
