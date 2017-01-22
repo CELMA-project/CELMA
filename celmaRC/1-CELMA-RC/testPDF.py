0581421792df3b80e6ca3b2ae943e53db3e1b7cb
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-22 15:46:51 +0100

    Small aesthetic fixes
    Former-commit-id: 8955b65beef628228bf7b9839718ba96d5abd336

--- celmaRC/1-CELMA-RC/testPDF.py
+++ celmaRC/1-CELMA-RC/testPDF.py
@@ -71,12 +71,18 @@ def driverTest():
     Runs the test for the probability density functions
     """
 
-    dmp_folders  = ("CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1",)
+    dmp_folders  = (    "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1",)
+
     collectPaths =\
        (\
-        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
-        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_7.8633_geom_Ly_275.2144_input_B0_0.1_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/"\
+                        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
+                        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/",\
+                        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/",\
        )
+
+    steadyStatePath   = "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/"
+
+    tSlice            = slice(1200,None)
 
     useSubProcess = False
 
@@ -87,11 +93,8 @@ def driverTest():
     xInd              = None
     yInd              = 16
     zInd              = 128
-    tSlice            = None
     nPoints           = 3
     equallySpace      = "x"
-
-    steadyStatePath   = "CSDXMagFieldScanAr/nout_2_timestep_50/nz_256/geom_Lx_4.718_geom_Ly_165.1286_input_B0_0.06_ownFilters_type_none_switch_useHyperViscAzVortD_False_tag_CSDXMagFieldScanAr-1-expand_0/"
 
     indicesArgs   = (xInd, yInd, zInd)
     indicesKwargs = {"tSlice"          : tSlice         ,\
