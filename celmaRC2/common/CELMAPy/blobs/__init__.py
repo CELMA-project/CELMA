c6bac276515a1c7d4504032777df87e1f69a3b73
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-23 11:07:24 +0100

    Made time statistics
    Former-commit-id: 01e0a3cfa8be81a2eb0a9dfcb9679c03b9fa25cb

--- celmaRC2/common/CELMAPy/blobs/__init__.py
+++ celmaRC2/common/CELMAPy/blobs/__init__.py
@@ -5,5 +5,11 @@ Init-file for blobs
 """
 
 from .collectAndCalcBlobs import CollectAndCalcBlobs
-from .driverBlobs import DriverBlobs, driverBlobs
-from .plotBlobs import PlotBlobs
+from .driverBlobs import (DriverBlobs           ,\
+                          driverPlot2DData      ,\
+                          driverTimeTraces      ,\
+                          driverWaitingTimePulse,\
+                          get2DData             ,\
+                          prepareBlobs          ,\
+                          )
+from .plotBlobs import PlotBlobs, PlotTemporalStats
