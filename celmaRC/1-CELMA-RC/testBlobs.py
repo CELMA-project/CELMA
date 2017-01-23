cd5cf892712dff604b3dc98b345e9d7f8a901521
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-23 17:02:07 +0100

    Transfer checkpoint
    Former-commit-id: e47478713fa63b8cc672f33ba149b8b9e0b6c8ae

--- celmaRC/1-CELMA-RC/testBlobs.py
+++ celmaRC/1-CELMA-RC/testBlobs.py
@@ -10,8 +10,8 @@ sys.path.append(commonDir)
 
 from CELMAPy.blobs import DriverBlobs, prepareBlobs, driverWaitingTimePulse
 
-#{{{testWaitingTimeAndPulseWidth
-def testWaitingTimeAndPulseWidth():
+#{{{testWaitingTimePulse
+def testWaitingTimePulse():
     """
     Runs the test for the blobs
     """
@@ -49,6 +49,48 @@ def testWaitingTimeAndPulseWidth():
                        convertToPhysical,\
                       )
     driverWaitingTimePulse(ccb, plotSuperKwargs)
+    print("Success!\n\n")
+#}}}
+
+#{{{testBlobTimeTrace
+def testBlobTimeTrace():
+    """
+    Runs the test for the blobs
+    """
+
+    collectPaths =\
+       (\
+        "CSDXMagFieldScanAr/nout_1000_timestep_1/geom_Lx_6.2906_geom_Ly_220.1715_input_B0_0.08_switch_forceAddNoise_False_switch_includeNoise_False_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-2-linearPhase1_0/",\
+        "CSDXMagFieldScanAr/nout_5000_timestep_1/geom_Lx_6.2906_geom_Ly_220.1715_input_B0_0.08_switch_saveTerms_False_switch_useHyperViscAzVortD_True_tag_CSDXMagFieldScanAr-3-turbulentPhase1_0/restart_1/",\
+       )
+
+    xInd   = 26
+    yInd   = 16
+    zInd   = 0
+    tSlice = slice(1000,None)
+    slices = (xInd, yInd, zInd, tSlice)
+    pctPadding = 400
+
+    convertToPhysical = True
+
+    savePath          = "."
+
+    plotSuperKwargs = {\
+                        "showPlot"     : False,\
+                        "savePlot"     : True,\
+                        "savePath"     : savePath,\
+                        "savePathFunc" : None,\
+                        "extension"    : None,\
+                        "dmp_folders"  : None,\
+                       }
+
+    print("\n\nTesting blobs")
+    ccb = prepareBlobs(collectPaths     ,\
+                       slices           ,\
+                       pctPadding       ,\
+                       convertToPhysical,\
+                      )
+    driverBlobTimeTrace(ccb, plotSuperKwargs)
     print("Success!\n\n")
 #}}}
 
