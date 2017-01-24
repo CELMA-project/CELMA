6b36f4edcc04d136b5a3120e5919702e5951a783
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-24 13:22:05 +0100

    Added the magnitude spectrum
    Former-commit-id: b06f625712c3ddf8b61d24b34d125afc05a264ab

---
+++ celmaRC2/common/CELMAPy/magnitueSpectrum/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+Init-file for magnitude spectrum
+"""
+
+from .collectAndCalcMagnitudeSpectrum import CollectAndCalcMagnitudeSpectrum
+from .driverMagnitudeSpectrum import (DriverMagnitudeSpectrum,\
+                                      driverMagnitudeSpectrum,\
+                                     )
+from .plotMagnitudeSpectrum import PlotMagnitudeSpectrum
---
+++ celmaRC2/common/CELMAPy/magnitueSpectrum/driverMagnitudeSpectrum.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+
+"""
+Contains single driver and driver class for the magnitude spectrum
+"""
+
+from ..superClasses import DriverPointsSuperClass
+from .collectAndCalcMagnitudeSpectrum import CollectAndCalcMagnitudeSpectrum
+from .plotMagnitudeSpectrum import PlotMagnitudeSpectrum
+from multiprocessing import Process
+
+#{{{driverMagnitudeSpectrum
+def driverMagnitudeSpectrum(collectPaths     ,\
+                            varName          ,\
+                            convertToPhysical,\
+                            indicesArgs      ,\
+                            indicesKwargs    ,\
+                            plotSuperKwargs  ,\
+                            ):
+    #{{{docstring
+    """
+    Driver for plotting magnitude spectrum.
+
+    Parameters
+    ----------
+    collectPaths : tuple
+        Paths to collect from.
+        The corresponind 't_array' of the paths must be in ascending order.
+    varName : str
+        The variable name which will be used.
+    convertToPhysical : bool
+        Whether or not to convert to physical units.
+    indicesArgs : tuple
+        Contains xInd, yInd and zInd.
+        See CollectAndCalcPointsSuperClass.setIndices for details.
+    indicesKwargs : dict
+        Contains tslice, nPoints, equallySpace and steadyStatePath.
+        See CollectAndCalcPointsSuperClass.setIndices for details.
+    plotSuperKwargs : dict
+        Keyword arguments for the plot super class.
+    """
+    #}}}
+
+    # Create collect object
+    ccms = CollectAndCalcMagnitudeSpectrum(\
+                             collectPaths                         ,\
+                             convertToPhysical = convertToPhysical,\
+                            )
+
+    # Execute the collection
+    fm, positionTuple, uc = ccms.executeCollectAndCalc()
+
+    import pdb; pdb.set_trace()
+    a = 1
+#    # Plot
+#    pfm = PlotMagnitudeSpectrum(ccms.uc         ,\
+#                           **plotSuperKwargs)
+#    pfm.setData(fm, nModes, timeAx = True)
+#    pfm.plotSaveShowMagnitudeSpectrum()
+#    pfm.setData(fm, nModes, timeAx = False)
+#    pfm.plotSaveShowMagnitudeSpectrum()
+#}}}
+
+#{{{DriverMagnitudeSpectrum
+class DriverMagnitudeSpectrum(DriverPointsSuperClass):
+    """
+    Class for driving of the plotting of the magnitude spectrum.
+    """
+
+    #{{{Constructor
+    def __init__(self                   ,\
+                 dmp_folders            ,\
+                 indicesArgs            ,\
+                 indicesKwargs          ,\
+                 plotSuperKwargs        ,\
+                 varName           = "n",\
+                 nModes            = 7  ,\
+                 **kwargs):
+        #{{{docstring
+        """
+        This constructor:
+            * Calls the parent class
+            * Set the member data
+            * Updates the plotSuperKwargs
+
+        Parameters
+        ----------
+        dmp_folders : tuple
+            Tuple of the dmp_folder (output from bout_runners).
+        indicesArgs : tuple
+            Contains xInd, yInd and zInd.
+            See CollectAndCalcPointsSuperClass.setIndices for details.
+        indicesKwargs : dict
+            Contains tslice, nPoints, equallySpace and steadyStatePath.
+            See CollectAndCalcPointsSuperClass.setIndices for details.
+        plotSuperKwargs : dict
+            Keyword arguments for the plot super class.
+        varName : str
+            Name of variable to collect and plot
+        nModes : int
+            Number of modes to plot.
+        **kwargs : keyword arguments
+            See parent class for details.
+        """
+        #}}}
+
+        # Call the constructor of the parent class
+        super().__init__(dmp_folders, **kwargs)
+
+        # Set the member data
+        self._varName       = varName
+        self._nModes        = nModes
+        self._indicesArgs   = indicesArgs
+        self._indicesKwargs = indicesKwargs
+
+        # Update the plotSuperKwargs dict
+        plotSuperKwargs.update({"dmp_folders":dmp_folders})
+        plotSuperKwargs.update({"plotType"   :"magnitudeSpectrum"})
+        self._plotSuperKwargs = plotSuperKwargs
+    #}}}
+
+    #{{{driverFourierMode
+    def driverFourierMode(self):
+        #{{{docstring
+        """
+        Wrapper to driverFourierMode
+        """
+        #}}}
+        args =  (\
+                 self._collectPaths    ,\
+                 self._varName         ,\
+                 self.convertToPhysical,\
+                 self._nModes          ,\
+                 self._indicesArgs     ,\
+                 self._indicesKwargs   ,\
+                 self._plotSuperKwargs ,\
+                )
+        if self._useSubProcess:
+            processes = Process(target = driverMagnitudeSpectrum, args = args)
+            processes.start()
+        else:
+            driverMagnitudeSpectrum(*args)
+    #}}}
+#}}}
