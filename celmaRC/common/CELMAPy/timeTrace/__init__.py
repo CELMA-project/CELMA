2c5c2092c422ea2443700785b7c5260f44037c64
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-25 22:43:51 +0100

    Completed symlinking
    Former-commit-id: 3deab0c789cfc389e66a45687d69ea4044c79cb7

--- celmaRC/common/CELMAPy/timeTrace/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Init-file for timeTrace
-"""
-
-from .collectAndCalcTimeTrace import CollectAndCalcTimeTrace
-from .driverTimeTrace import DriverTimeTrace, driverTimeTrace, getTimeTrace
-from .plotTimeTrace import PlotTimeTrace
