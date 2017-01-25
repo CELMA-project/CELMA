af5db9206b6337d95fca396d2f4c17d24bd2c70a
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-25 22:43:51 +0100

    Completed symlinking

--- celmaRC/common/CELMAPy/energy/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Init-file for energy
-"""
-
-from .collectAndCalcEnergy import CollectAndCalcEnergy
-from .driverEnergy import DriverEnergy, driverEnergy
-from .plotEnergy import PlotEnergy
--- celmaRC/common/CELMAPy/performance/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""
-Init-file for performance
-"""
-
-from .collectAndCalcPerformance import CollectAndCalcPerformance
-from .driverPerformance import DriverPerformance, driverPerformance
-from .plotPerformance import PlotPerformance
