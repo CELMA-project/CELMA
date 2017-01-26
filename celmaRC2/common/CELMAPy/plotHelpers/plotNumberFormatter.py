a7852e17f20135d858121662fc7990205e67a518
Michael Løiten <michael.l.magnussen@gmail.com>
2017-01-26 09:04:25 +0100

    Bugfix to formatter
    Former-commit-id: 20bee633c6d755afb644c34c8a801fbb154d7bf2

--- celmaRC2/common/CELMAPy/plotHelpers/plotNumberFormatter.py
+++ celmaRC2/common/CELMAPy/plotHelpers/plotNumberFormatter.py
@@ -21,10 +21,16 @@ def plotNumberFormatter(val, pos, precision=3):
     # Special case if 0.000x or 0.00x
     if "0.000" in tickString:
         tickString = tickString.replace("0.000", "")
-        tickString = "{}.{}e-03".format(tickString[0:2], tickString[2:])
+        if tickString[0] == "-":
+            tickString = "{}.{}e-03".format(tickString[0:2], tickString[2:])
+        else:
+            tickString = "{}.{}e-03".format(tickString[0:1], tickString[1:])
     if "0.00" in tickString:
         tickString = tickString.replace("0.00", "")
-        tickString = "{}.{}e-02".format(tickString[0:2], tickString[2:])
+        if tickString[0] == "-":
+            tickString = "{}.{}e-02".format(tickString[0:2], tickString[2:])
+        else:
+            tickString = "{}.{}e-02".format(tickString[0:1], tickString[1:])
     if "e+" in tickString:
         tickString = tickString.replace("e+0", r"\cdot 10^{")
         tickString = tickString.replace("e+" , r"\cdot 10^{")
