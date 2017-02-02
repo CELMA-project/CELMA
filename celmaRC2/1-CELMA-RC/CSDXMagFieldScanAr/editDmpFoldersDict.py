9ae1e6754e6adac6976d4050cc8fca239b8761dd
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-02 18:25:46 +0100

    Bugfix: Sorting of extra turb in post-processing
    Former-commit-id: fde2403792e82982f434b20ae5a77fef0a65e493

--- celmaRC2/1-CELMA-RC/CSDXMagFieldScanAr/editDmpFoldersDict.py
+++ celmaRC2/1-CELMA-RC/CSDXMagFieldScanAr/editDmpFoldersDict.py
@@ -9,20 +9,21 @@ Nifty when not all jobs have finished
 import pickle
 
 with open("dmpFoldersDict.pickle", "rb") as f:
-    a = pickle.load(f)
+    dmpFolders = pickle.load(f)
 
-a["extraTurbulence"] = list(a["extraTurbulence"])
-for nr in range(len(a["extraTurbulence"])):
-    a["extraTurbulence"][nr] = list(a["extraTurbulence"][nr])
-    a["extraTurbulence"][nr].pop(-1)
-    a["extraTurbulence"][nr] = tuple(a["extraTurbulence"][nr])
-a["extraTurbulence"] = tuple(a["extraTurbulence"])
+dmpFolders["extraTurbulence"] = list(dmpFolders["extraTurbulence"])
+for nr in range(len(dmpFolders["extraTurbulence"])):
+    dmpFolders["extraTurbulence"][nr] = list(dmpFolders["extraTurbulence"][nr])
+    # Remove the current running folder
+    dmpFolders["extraTurbulence"][nr].pop(-1)
+    dmpFolders["extraTurbulence"][nr] = tuple(dmpFolders["extraTurbulence"][nr])
+dmpFolders["extraTurbulence"] = tuple(dmpFolders["extraTurbulence"])
 
-a["turbulence"] = list(a["turbulence"])
-for nr in range(len(a["turbulence"])):
-    a["turbulence"][nr] += "/restart_3"
-    print(a["turbulence"][nr])
-a["turbulence"] = tuple(a["turbulence"])
+dmpFolders["turbulence"] = list(dmpFolders["turbulence"])
+for nr in range(len(dmpFolders["turbulence"])):
+    # Set the last finished folder to the last finished folder
+    dmpFolders["turbulence"][nr] += "/restart_5"
+dmpFolders["turbulence"] = tuple(dmpFolders["turbulence"])
 
 with open("dmpFoldersDict.pickle", "wb") as f:
-    pickle.dump(a,f)
+    pickle.dump(dmpFolders,f)
