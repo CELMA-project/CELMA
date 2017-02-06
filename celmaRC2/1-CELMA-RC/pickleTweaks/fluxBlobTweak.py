5ae7e1b05459e003f2edd42a23d9efc2aa067b38
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-06 10:59:09 +0100

    Added blobTweaks
    Former-commit-id: eb951bf1b08f22fb141ee7725d663bda8960b804

--- celmaRC2/1-CELMA-RC/pickleTweaks/fluxBlobTweak.py
+++ celmaRC2/1-CELMA-RC/pickleTweaks/fluxBlobTweak.py
@@ -7,6 +7,7 @@ Tweaks the flux obtained from the blob runner.
 import pickle
 import matplotlib.pylab as plt
 import numpy as np
+from scipy import stats
 
 import os, sys
 # If we add to sys.path, then it must be an absolute path
@@ -15,6 +16,11 @@ commonDir = os.path.abspath("./../../common")
 sys.path.append(commonDir)
 
 from CELMAPy.plotHelpers import PlotHelper, seqCMap2
+
+# Will not use namedTuple here as it is unnecessary complicated to
+# pickle/unpickle
+sigMul = (2,3,4)
+PDFStats={"stdDev":{key:None for key in sigMul}, "skew":None, "kurtExcess":None}
 
 scan = "B0_0.08"
 path = "../CSDXMagFieldScanAr/visualizationPhysical/{}/blobs/".format(scan)
@@ -33,21 +39,37 @@ fig.suptitle(label[0])
 leg = ax.legend()
 leg.remove()
 
+# Recolor line
 l = ax.get_lines()[0]
 l.set_color("k")
 time, flux = l.get_data()
 t = (time[0], time[-1])
-stdDev = flux.std()
+
+# Set the label colors
 colors = seqCMap2(np.linspace(0.25,0.75,3))
-for mul, color in zip((2,3,4), colors):
+
+# Calculate the standard deviation
+stdDev = flux.std()
+
+for mul, color in zip(sigMul, colors):
     fileName = os.path.join(path, str(mul), "counts.pickle")
     with open(fileName, "rb") as f:
         counts = pickle.load(f)
-    cond = (stdDev*mul,)*2
+    curSig = stdDev*mul
+    cond = (curSig,)*2
     label = r"${}\sigma$".format(mul)
     label += "\n" + "$\mathrm{{Blobs}}:{}$".format(counts.blobs)
     label += "\n" + "$\mathrm{{Holes}}:{}$".format(counts.holes)
     ax.plot(t,cond, alpha = 0.7, lw=2, label=label, color=color)
+    PDFStats["stdDev"][mul] = curSig
+
+# Pickle the PDFStats
+PDFStats["skew"]       = stats.skew(flux)
+PDFStats["kurtExcess"] = stats.kurtosis(flux)
+fileName = os.path.join(path, "3", "PDFStats.pickle")
+with open(fileName, "wb") as f:
+    pickle.dump(PDFStats, f, pickle.HIGHEST_PROTOCOL)
+    print("Pickled {}".format(fileName))
 
 # Move legend outside
 handles, labels = ax.get_legend_handles_labels()
@@ -67,4 +89,4 @@ fig.legend(handles[::-1],\
 fig.set_figwidth (4.0)
 fig.set_figheight(2.5)
 
-PlotHelper.savePlot(fig, "{}.pdf".format(scan))
+PlotHelper.savePlot(fig, "blobFluxTimeTrace_{}.pdf".format(scan))
