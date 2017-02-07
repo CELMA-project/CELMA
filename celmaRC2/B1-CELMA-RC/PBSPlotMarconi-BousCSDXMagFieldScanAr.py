a47277a3c6c903d155cd21e78fb26a8a4809e020
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-07 20:01:45 +0100

    Modified the drivers
    Former-commit-id: 6318ca1cd29828952775ffc765eb688b5b27c10b

--- celmaRC2/B1-CELMA-RC/PBSPlotMarconi-BousCSDXMagFieldScanAr.py
+++ celmaRC2/B1-CELMA-RC/PBSPlotMarconi-BousCSDXMagFieldScanAr.py
@@ -2,6 +2,8 @@
 
 """Driver which plots the results of the simulations."""
 
+import numpy as np
+
 import os, sys
 # If we add to sys.path, then it must be an absolute path
 commonDir = os.path.abspath("./../common")
@@ -10,14 +12,18 @@ sys.path.append(commonDir)
 
 from standardPlots import PlotSubmitter
 
-directory = "CSDXMagFieldScanAr"
+directory = "BousCSDXMagFieldScanAr"
 scanParameter = "B0"
 
 # Create the plotSubmitter
 pltSub = PlotSubmitter(directory, scanParameter, boussinesq=True)
-pltSub.sub.setMisc(logPath = os.path.join(directory,"postLogs"))
+pltSub.sub.setMisc(logPath = os.path.join(directory,"postLogs"),\
+                   mail    = "mmag@fysik.dtu.dk",\
+                   account = "FUA11_SOLF")
+pltSub.sub.setQueue("xfualongprod")
 
-# Set linear slices
+# Set linear slices (found from looking at the Fourier modes)
+# FIXME:
 tSlices = {\
            "B0_0.02":None,\
            "B0_0.04":None,\
@@ -27,7 +33,8 @@ tSlices = {\
            }
 pltSub.setLinearPhaseTSlices(tSlices)
 
-# Set saturated turbulence slices
+# Set saturated turbulence slices (found from looking at the energies)
+# FIXME:
 tSlices = {\
            "B0_0.02":None,\
            "B0_0.04":None,\
@@ -39,24 +46,50 @@ pltSub.setSatTurbTSlices(tSlices)
 
 # Run the post-processing
 pltSub.updatePlotSuperKwargs({"extension" : "pdf"})
+pltSub.runAnalyticGrowthRates()
+
+pltSub.runBlobs(modes="perp", fluct=True, condition=3)
+pltSub.runBlobs(modes="perp", fluct=True, condition=2)
+pltSub.runBlobs(modes="perp", fluct=True, condition=4)
+
+pltSub.runBlobDensPDF()
+
 pltSub.runCominedPlots()
 pltSub.runEnergy(sliced=False)
 pltSub.runEnergy(sliced=True)
 pltSub.runFourierModes(sliced=False)
 pltSub.runFourierModes(sliced=True)
 pltSub.runGrowthRates()
+pltSub.runMagnitudeSpectrum()
 pltSub.runPerformance(allFolders=False)
 pltSub.runPerformance(allFolders=True)
+pltSub.runPhaseShift()
 pltSub.runPosOfFluct()
 pltSub.runPSD2D()
 pltSub.runSkewKurt()
+pltSub.runSteadyState()
 pltSub.runZonalFlow()
+pltSub.runTotalFlux()
 
 # Run the animations
 pltSub.updatePlotSuperKwargs({"extension" : None})
-pltSub.sub.setWalltime("01:00:00")
 pltSub.runFields1DAnim()
-pltSub.sub.setQueue("fatq")
 pltSub.sub.setWalltime("06:00:00")
 pltSub.runFields2DAnim(fluct=True)
 pltSub.runFields2DAnim(fluct=False)
+
+# Snapshots plot
+# Obtain frames to see evolution
+# FIXME: (Maybe ok values to use, but check this)
+# NOTE: Plot 4,6 and 8 are chosen
+# B=0.01
+start   = 2055
+end     = 2155
+pics    = 10
+frameNr = np.linspace(start, end, pics)
+# Run runSnapShotsSameScanVal without vMaxVmin in order to see maxMin
+# FIXME:
+maxMin = (1.35e18, 7.50e18)
+maxMin = None
+turbSlices = tuple(slice(int(frame),int(frame)) for frame in frameNr)
+pltSub.runSnapShotsSameScanVal("param0",turbSlices,fluct=False,vMaxVMin=maxMin)
