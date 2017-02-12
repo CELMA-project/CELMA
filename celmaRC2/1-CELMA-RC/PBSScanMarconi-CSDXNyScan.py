d6d612befed228a421a767d6e6a0ad9f744dc3bd
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-12 13:49:41 +0100

    Fixed PBSScanMarconi-CSDXNyScan

--- celmaRC2/1-CELMA-RC/PBSScanMarconi-CSDXNyScan.py
+++ celmaRC2/1-CELMA-RC/PBSScanMarconi-CSDXNyScan.py
@@ -6,17 +6,26 @@ Note: This is done in the old school bout_runners way
 """
 
 from bout_runners import PBS_runner
+import numpy as np
 
 mode       = "initial"
 saveFolder = mode
 
-
 # Calculate the ny parameters
 ly   = 2.8
 rhos = 1.6956e-02 # Copied from logfiles
+ratios = np.linspace(0.03,0.3,5)
 # NOTE: ny=66 is the standard
-nys  = tuple(int(ny) for ny in a*(ly/rhos))
+nys  = tuple(int(ny) for ny in ratios*(ly/rhos))
 
+PBSOptions = {\
+              "BOUT_walltime" : "72:00:00"  ,\
+              "BOUT_queue"    : "xfualong"  ,\
+              "BOUT_account"  : "FUA11_SOLF",\
+              "BOUT_ppn"      : 36          ,\
+             }
+
+nproc = 32
 if mode == "initial":
     #{{{initial
     # The options for the run
@@ -24,8 +33,10 @@ if mode == "initial":
     # Set the temporal domain
     restart    = None
     remove_old = False
-    nout       = [2]
-    timestep   = [2e3]
+    tempOptions = {\
+                   "nout"     : 2  ,\
+                   "timestep" : 2e3,\
+                  }
     directory  = "CSDXNyScan"
     # Shall we make?
     make       = False
@@ -34,44 +45,95 @@ if mode == "initial":
     # The PBS options
     # ==========================================================================
     # Specify the numbers used for the BOUT runs
-# FIXME:
-    nproc                 = 96
-    BOUT_nodes            = 1
-    BOUT_ppn              = 20
-    BOUT_walltime         = "72:00:00"
-    BOUT_queue            = "xfualong"
-    BOUT_run_name         = saveFolder
+    PBSOptions["BOUT_run_name"] = saveFolder
     # ==========================================================================
 
-    # Create the runner
-    # ==========================================================================
-    myRuns = PBS_runner(\
-                directory  = directory ,\
-                nproc      = nproc ,\
-                # Set temporal domain
-                nout       = nout  ,\
-                timestep   = timestep,\
-                # Copy the source file
-                cpy_source = True  ,\
-                make       = make  ,\
-                restart    = restart,\
-                # PBS options
-                BOUT_nodes            = BOUT_nodes   ,\
-                BOUT_ppn              = BOUT_ppn     ,\
-                BOUT_queue            = BOUT_queue   ,\
-                BOUT_walltime         = BOUT_walltime,\
-                BOUT_run_name         = BOUT_run_name,\
-                # Tag (used to catalogize the runs)
-                additional = ("tag",saveFolder,0)    ,\
-                )
-    # ==========================================================================
+    for ny in nys:
+        while ny%2 != 0:
+            ny += 1
 
-    # Perform the run
-    # ==========================================================================
-    myRuns.execute_runs(remove_old = remove_old)
-    # ==========================================================================
+        PBSOptions["BOUT_nodes"] = int(np.ceil(nproc/PBSOptions["BOUT_ppn"]))
+
+        # Create the runner
+        # ======================================================================
+        myRuns = PBS_runner(\
+                    directory  = directory,\
+                    nproc      = nproc    ,\
+                    # Spatial options
+                    ny         = ny       ,\
+                    nz         = 1        ,\
+                    # Set temporal domain
+                    **tempOptions         ,\
+                    # Copy the source file
+                    cpy_source = True     ,\
+                    make       = make     ,\
+                    restart    = restart  ,\
+                    # PBS options
+                    **PBSOptions          ,\
+                    # Tag (used to catalogize the runs)
+                    additional = ("tag",saveFolder,0),\
+                    )
+        # ======================================================================
+
+        # Perform the run
+        # ======================================================================
+        myRuns.execute_runs(remove_old = remove_old)
+        # ======================================================================
     #}}}
 elif mode == "steadyState":
     #{{{steadyState
-    raise NotImplementedError("steadyState is to be implemented")
+    # The options for the run
+    # ==========================================================================
+    # Set the temporal domain
+    restart    = "overwrite"
+    remove_old = False
+    tempOptions = {\
+                   "nout"     : 2 ,\
+                   "timestep" : 25,\
+                  }
+    directory  = "CSDXNyScan"
+    # Shall we make?
+    make       = False
+    # ==========================================================================
+
+    # The PBS options
+    # ==========================================================================
+    # Specify the numbers used for the BOUT runs
+    PBSOptions["BOUT_run_name"] = saveFolder
+    # ==========================================================================
+
+    for ny in nys:
+        while ny%2 != 0:
+            ny += 1
+        restart_from = ("CSDXNyScan/nout_2_timestep_2000.0/"
+                        "ny_{}_nz_1/tag_initial_0/").format(ny)
+
+        PBSOptions["BOUT_nodes"] = int(np.ceil(nproc/PBSOptions["BOUT_ppn"]))
+
+        # Create the runner
+        # ======================================================================
+        myRuns = PBS_runner(\
+                    directory  = directory     ,\
+                    nproc      = nproc         ,\
+                    # Spatial options
+                    ny         = ny            ,\
+                    nz         = 256           ,\
+                    # Set temporal domain
+                    **tempOptions              ,\
+                    # Copy the source file
+                    cpy_source   = True        ,\
+                    make         = make        ,\
+                    restart      = restart     ,\
+                    restart_from = restart_from,\
+                    # PBS options
+                    **PBSOptions               ,\
+                    # Tag (used to catalogize the runs)
+                    additional = ("tag",saveFolder,0),\
+                    )
+        # ======================================================================
+
+        # Perform the run
+        # ======================================================================
+        myRuns.execute_runs(remove_old = remove_old)
+        # ======================================================================
     #}}}
