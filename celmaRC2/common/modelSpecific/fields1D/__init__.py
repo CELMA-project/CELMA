a51793757d4f1ac3bea62836c144a4bd109b7411
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-17 13:20:00 +0100

    Removed spurios modelSpecific folder
    Former-commit-id: 494606c326ae3d9541b077b2b961b1073ced4291

--- celmaRC2/common/modelSpecific/fields1D/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-""" Init-file for the fields 1D """
-
-from .collectAndCalcFields1D import CollectAndCalcFields1D
-from .driverFields1D import driver1DFieldSingle, Driver1DFields
-from .plotFields1D import PlotAnim1DRadial, PlotAnim1DParallel
