cfd0e05d55cffc3e1534b35083de27d619f8948b
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-21 19:20:02 +0100

    Fixed runners

--- celmaRC2/1-CELMA-RC/PBSPlotMarconi-CSDXNeutralScanAr.py
+++ celmaRC2/1-CELMA-RC/PBSPlotMarconi-CSDXNeutralScanAr.py
@@ -15,46 +15,102 @@ scanParameter = "nn"
 
 # Create the plotSubmitter
 pltSub = PlotSubmitter(directory, scanParameter)
-pltSub.sub.setMisc(logPath = os.path.join(directory,"postLogs"))
+pltSub.sub.setMisc(logPath = os.path.join(directory,"postLogs"),\
+                   mail    = "mmag@fysik.dtu.dk",\
+                   account = "FUA11_SOLF")
+pltSub.sub.setQueue("xfualongprod")
 
-# Set linear slices
+# Set linear slices (found from looking at the Fourier modes)
 tSlices = {\
-           "nn_5e+19"                 : None,\
-           "nn_2.5e+19"               : None,\
-           "nn_1.6666666666666668e+19": None,\
-           "nn_1.25e+19"              : None,\
-           }
+    "nn_2.5e+18"               : slice(50, 300),\
+    "nn_6.666666666666668e+18" : slice(50, 225),\
+    "nn_1.5e+19"               : slice(50, 210),\
+    "nn_4e+19"                 : slice(50, 210),\
+    "nn_9.9e+20"               : slice(50, 500),\
+          }
 pltSub.setLinearPhaseTSlices(tSlices)
 
-# Set saturated turbulence slices
+# Set saturated turbulence slices (found from looking at the energies)
 tSlices = {\
-           "nn_5e+19"                 : None,\
-           "nn_2.5e+19"               : None,\
-           "nn_1.6666666666666668e+19": None,\
-           "nn_1.25e+19"              : None,\
+    "nn_2.5e+18"               : slice(int(1.5e3), None),\
+    "nn_6.666666666666668e+18" : slice(int(1.5e3), None),\
+    "nn_1.5e+19"               : slice(int(1.5e3), None),\
+    "nn_4e+19"                 : slice(int(1.5e3), None),\
+    "nn_9.9e+20"               : slice(int(4.5e3), None),\
            }
 pltSub.setSatTurbTSlices(tSlices)
 
 # Run the post-processing
 pltSub.updatePlotSuperKwargs({"extension" : "pdf"})
+
+pltSub.runBlobs(modes="perp", fluct=True, condition=3)
+pltSub.runBlobs(modes="perp", fluct=True, condition=2)
+pltSub.runBlobs(modes="perp", fluct=True, condition=4)
+
+pltSub.runBlobDensPDF()
+
 pltSub.runCominedPlots()
 pltSub.runEnergy(sliced=False)
 pltSub.runEnergy(sliced=True)
 pltSub.runFourierModes(sliced=False)
 pltSub.runFourierModes(sliced=True)
 pltSub.runGrowthRates()
+pltSub.runMagnitudeSpectrum()
 pltSub.runPerformance(allFolders=False)
 pltSub.runPerformance(allFolders=True)
+pltSub.runPhaseShift()
 pltSub.runPosOfFluct()
 pltSub.runPSD2D()
 pltSub.runSkewKurt()
+pltSub.runSteadyState()
 pltSub.runZonalFlow()
+pltSub.runTotalFlux()
 
 # Run the animations
 pltSub.updatePlotSuperKwargs({"extension" : None})
-pltSub.sub.setWalltime("01:00:00")
 pltSub.runFields1DAnim()
-pltSub.sub.setQueue("fatq")
 pltSub.sub.setWalltime("06:00:00")
 pltSub.runFields2DAnim(fluct=True)
 pltSub.runFields2DAnim(fluct=False)
+
+# Snapshots plot
+# Obtain evolution of the mode
+# FIXME
+modeSlices = (\
+              slice(0, 0),\
+              slice(0, 0),\
+              slice(0, 0),\
+              )
+pltSub.runSnapShotsSameScanVal("param0", modeSlices, fluct=True, yInd=50)
+
+# Obtain the different modes
+# FIXME
+modesSlices = {\
+               "nn_2.5e+18"               : None,\
+               "nn_6.666666666666668e+18" : None,\
+               "nn_1.5e+19"               : None,\
+               "nn_4e+19"                 : None,\
+               "nn_9.9e+20"               : None,\
+               }
+pltSub.runSnapShotDifferentScanVals(modesSlices, fluct=True, yInd=50)
+
+# Obtain frames to see evolution
+# FIXME
+# NOTE: Plot 5,6 and 7 are chosen
+# nn_9.9e+20
+start   = 2055
+end     = 2155
+pics    = 10
+frameNr = np.linspace(start, end, pics)
+# Run runSnapShotsSameScanVal without vMaxVmin in order to see maxMin
+# FIXME
+maxMin = (1.35e18, 7.50e18)
+turbSlices = tuple(slice(int(frame),int(frame)) for frame in frameNr)
+pltSub.runSnapShotsSameScanVal("param0",turbSlices,fluct=False,vMaxVMin=maxMin)
+
+# Obtain the turbulence fluctuations
+# FIXME
+turbSlices = (\
+              slice(2122, 2122),\
+             )
+pltSub.runSnapShotsSameScanVal("param0", turbSlices, fluct=True)
