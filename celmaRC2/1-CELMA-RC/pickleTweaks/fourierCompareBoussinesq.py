fc45c45e8a40784eaa92f80a15816049ac04403d
Michael Løiten <michael.l.magnussen@gmail.com>
2017-02-25 19:27:48 +0100

    Added new tweaks

---
+++ celmaRC2/1-CELMA-RC/pickleTweaks/fourierCompareBoussinesq.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Tweak of Fourier transformed time traces used in Boussinesq compare.
+"""
+
+import pickle
+import matplotlib.pylab as plt
+import numpy as np
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath("./../../common")
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from CELMAPy.plotHelpers import PlotHelper
+
+picklePath = "../CSDXMagFieldScanAr/visualizationPhysical/B0_0.06/fourierModes/n-fourierModes-rho-0.0363-z-0.7.pickle"
+with open(picklePath, "rb") as f:
+    fig = pickle.load(f)
+
+_, height = fig.get_size_inches()
+width   = 3
+fig.set_size_inches(width, height)
+fileName = "FFT006.pdf"
+PlotHelper.savePlot(fig, fileName)
+plt.close(fig)
+
+picklePath = "../../B1-CELMA-RC/BousCSDXMagFieldScanAr/visualizationPhysical/B0_0.06/fourierModes/n-fourierModes-rho-0.0388-z-0.7.pickle"
+with open(picklePath, "rb") as f:
+    fig = pickle.load(f)
+
+fig.set_size_inches(width, height)
+fileName = "FFT006B.pdf"
+PlotHelper.savePlot(fig, fileName)
+plt.close(fig)
