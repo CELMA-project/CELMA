0477275c4942a271a5e1414c1ba81e4e70b0d230
Michael Løiten <michael.l.magnussen@gmail.com>
2017-03-28 14:42:11 +0200

    File clean-up
    Former-commit-id: 0fabe26796752b7677f434ee949ae52aa2f87959

---
+++ celma/testPostProcessing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""Driver which plots the results of the simulations."""
+
+import numpy as np
+
+import os, sys
+# If we add to sys.path, then it must be an absolute path
+commonDir = os.path.abspath("./../common")
+# Sys path is a list of system paths
+sys.path.append(commonDir)
+
+from standardPlots import PlotSubmitter
+
+directory = "CSDXMagFieldScanAr"
+scanParameter = "B0"
+
+# Create the plotSubmitter
+pltSub = PlotSubmitter(directory, scanParameter)
+
+# Toggle to not parallelize
+pltSub.sub.toggleSubmitOrRun()
+
+# Run the animations
+pltSub.runFields1DAnim()
+pltSub.runFields2DAnim(fluct=True)
+pltSub.runFields2DAnim(fluct=False)
