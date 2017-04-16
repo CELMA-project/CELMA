b4a2627a8915a5e46c85f099954be74f5edb2fce
Michael Løiten <michael.l.magnussen@gmail.com>
2017-04-16 23:36:32 +0200

    Added travis workaround to install script
    Former-commit-id: cfedf81a2e85d198ad6321393b650ce86abd68e1

--- install/boutPPInstall.sh
+++ install/boutPPInstall.sh
@@ -40,12 +40,40 @@ elif [ "$DEBUG" = "true" ]; then
     EXTRA_FLAGS="${EXTRA_FLAGS} --enable-debug --enable-checks=3 --enable-track"
 fi
 
+# Travis 4MB workaround
+# http://stackoverflow.com/questions/26082444/how-to-work-around-travis-cis-4mb-output-limit/26082445#26082445
+# ..............................................................................
+export PING_SLEEP=30s
+export CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export BUILD_OUTPUT=$CURDIR/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 500 lines of output:
+   tail -500 $BUILD_OUTPUT
+}
+error_handler() {
+  kill $PING_LOOP_PID
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+# ..............................................................................
+
 # Set the verbosity
 if [ "$VERBOSE" = "true" ]; then
     # Could also have used " > /dev/stdout"
     STDOUT=""
 else
-    STDOUT=" > /dev/null"
+    STDOUT=" &>> $BUILD_OUTPUT"
 fi
 # ==============================================================================
 
@@ -63,7 +91,7 @@ if [ "$INSTALL_CMAKE" = "true" ]; then
 fi
 
 if [ "$INSTALL_FFMPEG" = "true" ]; then
-    echo "$CURDIR/ffmpegInstall.sh $STDOUT"
+    echo "bash $CURDIR/ffmpegInstall.sh $STDOUT"
     bash $CURDIR/ffmpegInstall.sh $STDOUT
 fi
 
@@ -137,4 +165,16 @@ fi
 if [ "$INSTALL_CONDA" = true ]; then
     echo -e "export PATH=\"\$HOME/anaconda3/bin:\$PATH\""
 fi
+# ==============================================================================
+
+
+# Termination of travis workaround
+# ==============================================================================
+# The build finished without returning an error so dump a tail of the output
+if [ ! "$VERBOSE" = "true" ]; then
+    dump_output
+fi
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
 # ==============================================================================
