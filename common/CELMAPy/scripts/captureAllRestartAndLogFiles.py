3bd3016cbc8bdd4076b234eaac1057983d1914e5
Michael Løiten <michael.l.magnussen@gmail.com>
2017-11-26 15:59:22 +0100

    Modified captureAllRestartAndLogFiles

--- common/CELMAPy/scripts/captureAllRestartAndLogFiles.py
+++ common/CELMAPy/scripts/captureAllRestartAndLogFiles.py
@@ -1,58 +1,68 @@
 #!/usr/bin/env python
 
 """
-Saves all *.log.* and *.restart.* files of a directory to a zip.
-
-The folder structure will be preserved.
-The zip will be mailed if a mail is set.
+Contains the captureAllRestartAndLogFiles function.
 """
 
 import os, shutil, pathlib, subprocess
 
-# INPUT
-# =============================================================================
-# Directory to find log and restart files from
-directory = "CSDXMagFieldScanAr"
-# If you would like a mail on finished job enter your mail here
-# Example: "john@doe.com"
-mail = None
-# =============================================================================
+def captureAllRestartAndLogFiles(directory, mail=None):
+    """
+    Saves all *.log.* and *.restart.* files of a directory to a zip.
 
-logRestartDir = "logAndRestartFilesFrom" + directory
+    The folder structure will be preserved.
+    The zip will be mailed if a mail is set.
 
-# Find all the log files
-logFiles = list(pathlib.Path(directory).glob("**/*.log.*"))
+    Parameters
+    ----------
+    directory : str
+        Directory to recursively find log and restart files from.
+    mail : [None | str]
+        If set to a string, the zip will be mailed if mutt mailing is
+        supported.
+    """
 
-# Find all the restart files
-restartFiles = list(pathlib.Path(directory).glob("**/*.restart.*"))
+    if directory == ".":
+        logRestartDir = "logAndRestartFilesFromRoot"
+    else:
+        logRestartDir = "logAndRestartFilesFrom" + directory
 
-# Combine
-allFiles = (*logFiles, *restartFiles)
+    # Find all the log files
+    logFiles = list(pathlib.Path(directory).glob("**/*.log.*"))
 
-for f in allFiles:
-    # Add logRestartDir to directory without its root
-    dst = pathlib.PurePath(logRestartDir, f.relative_to(*f.parts[:2]))
-    # Clean folders marked with BAK
-    if "BAK" not in str(dst):
-        # Make the paths
-        pathlib.Path(*dst.parts[:-1]).mkdir(parents = True, exist_ok=True)
-        shutil.copy2(str(f), str(dst))
+    # Find all the restart files
+    restartFiles = list(pathlib.Path(directory).glob("**/*.restart.*"))
 
-# Compress and remove the folder
-shutil.make_archive(logRestartDir, "zip", logRestartDir)
-shutil.rmtree(logRestartDir)
+    # Combine
+    allFiles = (*logFiles, *restartFiles)
+
+    for f in allFiles:
+        # Add logRestartDir to directory without its root
+        dst = pathlib.PurePath(logRestartDir, f.relative_to(*f.parts[:2]))
+        # Clean folders marked with BAK
+        if "BAK" not in str(dst):
+            # Make the paths
+            pathlib.Path(*dst.parts[:-1]).mkdir(parents = True, exist_ok=True)
+            shutil.copy2(str(f), str(dst))
+
+    # Compress and remove the folder
+    shutil.make_archive(logRestartDir, "zip", logRestartDir)
+    shutil.rmtree(logRestartDir)
 
 
-if mail:
-    # Sends mail through the terminal
-    theZip = logRestartDir + ".zip"
-    cmd = (\
-        'echo "See attachment" | mutt -a "{}" -s "Log and restart files" -- {}'\
-          ).format(theZip, mail)
+    if mail:
+        # Sends mail through the terminal
+        theZip = logRestartDir + ".zip"
+        cmd = (\
+            'echo "See attachment" | mutt -a "{}" -s "Log and restart files" -- {}'\
+              ).format(theZip, mail)
 
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print("{} sent to {}".format(theZip, mail))
+        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        print("{} sent to {}".format(theZip, mail))
 
-    # Clean-up
-    shutil.rm(theZip)
+        # Clean-up
+        shutil.rm(theZip)
+
+if __name__ == "__main__":
+    captureAllRestartAndLogFiles(".")
