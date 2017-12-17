2bb3e6f353bf674b66ea1fb575709d14be356413
loeiten <michael.l.magnussen@gmail.com>
2017-12-17 23:43:42 +0100

    Found good hdf5 link

--- install/hdf5Install.sh
+++ install/hdf5Install.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# NOTE: No workaround for the new hdf5-1.10.1 has been found.
-#       Apparent download location:
-#       https://www.hdfgroup.org/downloads/hdf5/source-code/#
-#       Or login via
-#       https://www.hdfgroup.org/downloads/hdf5/
-
 # Installs hdf5
 HDF5_MAJOR="1"
-HDF5_MINOR="8"
-HDF5_PATCH="20"
+HDF5_MINOR="10"
+HDF5_PATCH="1"
 
 # exit on error
 set -e
@@ -26,7 +20,7 @@ mkdir -p examples
 cd ..
 mkdir -p install
 cd install
-wget https://support.hdfgroup.org/ftp/HDF5/current18/bin/hdf5-${HDF5_VERSION}-linux-centos7-x86_64-gcc485-shared.tar.gz -O hdf5-${HDF5_VERSION}.tar.gz
+https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_MAJOR_MINOR}/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz
 tar -xzvf hdf5-${HDF5_VERSION}.tar.gz
 cd hdf5-${HDF5_VERSION}
 # : is the no-op command
