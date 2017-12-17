42e79d528a8c81b0957c4367d8eda4a78a61ed5b
loeiten <michael.l.magnussen@gmail.com>
2017-12-17 23:41:01 +0100

    HDF5 download hassle

--- install/hdf5Install.sh
+++ install/hdf5Install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# NOTE: No workaround for the new hdf5-1.10.1 has been found.
+#       Apparent download location:
+#       https://www.hdfgroup.org/downloads/hdf5/source-code/#
+#       Or login via
+#       https://www.hdfgroup.org/downloads/hdf5/
+
 # Installs hdf5
 HDF5_MAJOR="1"
-HDF5_MINOR="10"
-HDF5_PATCH="1"
+HDF5_MINOR="8"
+HDF5_PATCH="20"
 
 # exit on error
 set -e
@@ -20,7 +26,7 @@ mkdir -p examples
 cd ..
 mkdir -p install
 cd install
-wget ftp://ftp.hdfgroup.org/HDF5/releases/hdf5-${HDF5_MAJOR_MINOR}/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz
+wget https://support.hdfgroup.org/ftp/HDF5/current18/bin/hdf5-${HDF5_VERSION}-linux-centos7-x86_64-gcc485-shared.tar.gz -O hdf5-${HDF5_VERSION}.tar.gz
 tar -xzvf hdf5-${HDF5_VERSION}.tar.gz
 cd hdf5-${HDF5_VERSION}
 # : is the no-op command
@@ -29,3 +35,4 @@ make clean || :
 make
 make install
 echo -e "\n\n\nDone installing hdf5\n\n\n"
+
